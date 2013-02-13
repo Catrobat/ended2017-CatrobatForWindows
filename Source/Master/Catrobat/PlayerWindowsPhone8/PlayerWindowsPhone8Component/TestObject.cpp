bcdb31e831752429ab9dd04b8719755f0383d8b5
hannes.hasenauer <CATROBAT\hannes.hasenauer>
2013-02-13 13:16:05 +0100

    Testing positioning

--- Source/Master/Catrobat/PlayerWindowsPhone8/PlayerWindowsPhone8Component/TestObject.cpp
+++ Source/Master/Catrobat/PlayerWindowsPhone8/PlayerWindowsPhone8Component/TestObject.cpp
@@ -10,14 +10,15 @@ void TestObject::Draw(SpriteBatch *spriteBatch)
 	spriteBatch->Draw(m_Texture, m_position, Colors::White);
 	XMFLOAT2 test1;
 	XMFLOAT2 test2;
+	XMFLOAT2 test3;
 	test1.x = 1.1f;
 	test1.y = 50.5f;
 	test2.x = 80.8f;
 	test2.y = 90.9f;
-	XMVECTOR test3 = {test1.x, test1.y , test2.x, test2.y};
-
+	test3.x = 80.8f;
+	test3.y = 90.9f;
 	
-	spriteBatch->Draw(m_Texture, test1, Colors::Wheat);
+	spriteBatch->Draw(m_Texture, test1, nullptr, Colors::Wheat, 0.0f, test2, test3, SpriteEffects_None, 0.0f);
 }
 
 void TestObject::LoadTexture(ID3D11Device* d3dDevice)
