dfd95beacdb175d8b0736993a66803140a31684e
hannes.hasenauer <CATROBAT\hannes.hasenauer>
2013-02-14 12:17:53 +0100

    getters for Project class

--- Source/Master/Catrobat/PlayerWindowsPhone8/PlayerWindowsPhone8Component/Project.cpp
+++ Source/Master/Catrobat/PlayerWindowsPhone8/PlayerWindowsPhone8Component/Project.cpp
@@ -2,9 +2,43 @@
 #include "Project.h"
 
 
-Project::Project(int version, int versionCode, int versionName, string projectName, int screenWidth, int screenHeight, SpriteList *spriteList) :
+Project::Project(int version, int versionCode, string versionName, string projectName, int screenWidth, int screenHeight, SpriteList *spriteList) :
 	m_version(version), m_versionCode(versionCode), m_versionName(versionName),
 	m_projectName(projectName), m_screenWidth(screenWidth), m_screenHeight(screenHeight), m_spriteList(spriteList)
 {
 }
 
+int Project::getVersion()
+{
+	return m_version;
+}
+
+int Project::getVersionCode()
+{
+	return m_versionCode;
+}
+
+string Project::getVersionName()
+{
+	return m_versionName;
+}
+
+string Project::getProjectName()
+{
+	return m_projectName;
+}
+
+int Project::getScreenWidth()
+{
+	return m_screenWidth;
+}
+
+int Project::getScreenHeight()
+{
+	return m_screenHeight;
+}
+
+SpriteList *Project::getSpriteList()
+{
+	return m_spriteList;
+}
--- Source/Master/Catrobat/PlayerWindowsPhone8/PlayerWindowsPhone8Component/Project.h
+++ Source/Master/Catrobat/PlayerWindowsPhone8/PlayerWindowsPhone8Component/Project.h
@@ -6,14 +6,22 @@
 class Project
 {
 public:
-	Project(int version, int versionCode, int versionName, string projectName, 
+	Project(int version, int versionCode, string versionName, string projectName, 
 		int screenWidth, int screenHeight, SpriteList *spriteList);
 	~Project();
+
+	int getVersion();
+	int getVersionCode();
+	string getVersionName();
+	string getProjectName();
+	int getScreenWidth();
+	int getScreenHeight();
+	SpriteList *getSpriteList();
 
 private:
 	int m_version;
 	int m_versionCode;
-	int m_versionName;
+	string m_versionName;
 	string m_projectName;
 	int m_screenWidth;
 	int m_screenHeight;
