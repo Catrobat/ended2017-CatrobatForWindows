f7752250e3c23682acba555bc7c4a6cb680de2eb
valentin.rock <CATROBAT\valentin.rock>
2013-02-15 18:20:47 +0100

    Created new ide phone project

---
+++ Source/Master/Catrobat/IDEWindowsPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using IDEWindowsPhone.Resources;
+
+namespace IDEWindowsPhone
+{
+  /// <summary>
+  /// Provides access to string resources.
+  /// </summary>
+  public class LocalizedStrings
+  {
+    private static AppResources _localizedResources = new AppResources();
+
+    public AppResources LocalizedResources { get { return _localizedResources; } }
+  }
+}
