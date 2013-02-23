113cdd32bfd9f401d12cd127660197bd0f938118
Valentin Rock <CATROBAT\valentin.rock>
2013-02-23 20:22:43 +0100

    Task 66

---
+++ Source/Master/Catrobat/IDEWindowsPhone/Controls/ListPicker/ListPickerPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Catrobat.IDEWindowsPhone.Controls.ListPicker
+{
+  public partial class ListPickerPage : PhoneApplicationPage
+  {
+    public static ListPicker ListPicker { get; set; }
+
+    public ListPickerPage()
+    {
+      InitializeComponent();
+    }
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+      _firstSelectionChanged = true;
+
+      ListBoxItems.ItemTemplate = ListPicker.PageItemTemplate;
+      ListBoxItems.ItemsSource = ListPicker.NullItemCollection;
+      ListBoxItems.SelectedItem = ListPicker.SelectedItem;
+      base.OnNavigatedTo(e);
+    }
+
+    private bool _firstSelectionChanged = true;
+    private void ListBoxItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+      if (_firstSelectionChanged)
+      {
+        _firstSelectionChanged = false;
+        return;
+      }
+
+      ListPicker.SelectedItem = ListBoxItems.SelectedItem;
+      var phoneApplicationFrame = Application.Current.RootVisual as PhoneApplicationFrame;
+      if (phoneApplicationFrame != null)
+        phoneApplicationFrame.GoBack();
+    }
+  }
+}
