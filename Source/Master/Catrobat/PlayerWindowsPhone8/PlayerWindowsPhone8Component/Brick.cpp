74cff36538f1492db4979b61aa135bdf55a10e28
hannes.hasenauer <CATROBAT\hannes.hasenauer>
2013-04-09 13:23:49 +0200

    Rollback to 171

--- Source/Master/Catrobat/PlayerWindowsPhone8/PlayerWindowsPhone8Component/Brick.cpp
+++ Source/Master/Catrobat/PlayerWindowsPhone8/PlayerWindowsPhone8Component/Brick.cpp
@@ -1,11 +1,10 @@
 #include "pch.h"
 #include "Brick.h"
-#include "Script.h"
 
-Brick::Brick(TypeOfBrick brickType, string spriteReference, Script* parent) :
+
+Brick::Brick(TypeOfBrick brickType, string spriteReference) :
 	BaseObject(200, 500, 1, 1, 0, 0),
-	m_brickType(brickType), m_spriteReference(spriteReference),
-	m_parent(parent)
+	m_brickType(brickType), m_spriteReference(spriteReference)
 {
 }
 
@@ -31,13 +30,6 @@ void Brick::Draw(SpriteBatch *spriteBatch)
 
 void Brick::LoadTexture(ID3D11Device* d3dDevice)
 {
-	
-	Windows::ApplicationModel::Package^ package = Windows::ApplicationModel::Package::Current;
-	Windows::Storage::StorageFolder^ installedLocation = package->InstalledLocation;
-	auto files = installedLocation->GetFilesAsync();
-	auto appInstallDirectory = Windows::Storage::ApplicationData::Current->LocalFolder;
-	Platform::String^ output = "Installed Location: " + installedLocation->Path;
-	CreateDDSTextureFromFile(d3dDevice, L"testProject/images/34A109A82231694B6FE09C216B390570_normalCat.dds", nullptr, &m_Texture, MAXSIZE_T);
-	//CreateDDSTextureFromFile(d3dDevice, L"brick.dds", nullptr, &m_Texture, MAXSIZE_T);
+	CreateDDSTextureFromFile(d3dDevice, L"brick.dds", nullptr, &m_Texture, MAXSIZE_T);
 }
 
