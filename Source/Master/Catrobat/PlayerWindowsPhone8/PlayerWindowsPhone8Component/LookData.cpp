1235c5c07a455fca591acbb4498f76986ca6f84e
Maximilian Sachs <CATROBAT\maximilian.sachs>
2013-04-09 15:17:59 +0200

    Dynamic Texture Loading...

--- Source/Master/Catrobat/PlayerWindowsPhone8/PlayerWindowsPhone8Component/LookData.cpp
+++ Source/Master/Catrobat/PlayerWindowsPhone8/PlayerWindowsPhone8Component/LookData.cpp
@@ -20,6 +20,7 @@ string LookData::FileName()
 
 void LookData::LoadTexture(ID3D11Device* d3dDevice)
 {
+	m_filename = "testProject/images/" + m_filename + ".dds";
 	std::wstring widestr = std::wstring(m_filename.begin(), m_filename.end());
 	const wchar_t* widecstr = widestr.c_str();
 	CreateDDSTextureFromFile(d3dDevice, widecstr, nullptr, &m_Texture, MAXSIZE_T);
