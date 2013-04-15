7f209b3934bad97e3d26621426b697f2c2158a5d
Max Schafzahl <CATROBAT\max.schafzahl>
2013-04-15 22:56:10 +0200

    Initial Paint App. We are starting with a standalone app. To perhaps be included some day.

---
+++ Source/Master/Catrobat/PaintApp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PaintApp.Resources;
+
+namespace PaintApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
