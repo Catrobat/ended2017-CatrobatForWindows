11760b6ad8691e0827a452cd0e02c16de1ae89dd
Maximilian Sachs <CATROBAT\maximilian.sachs>
2013-05-24 16:06:20 +0200

    Changed Evaluation from int to float (WaitBrick).

--- Source/Master/Catrobat/PlayerWindowsPhone8/PlayerWindowsPhone8Component/WaitBrick.cpp
+++ Source/Master/Catrobat/PlayerWindowsPhone8/PlayerWindowsPhone8Component/WaitBrick.cpp
@@ -11,5 +11,5 @@ WaitBrick::WaitBrick(string objectReference, FormulaTree *timeToWaitInSeconds, S
 
 void WaitBrick::Execute()
 {
-	Concurrency::wait(1000 * Interpreter::Instance()->EvaluateFormulaToInt(m_timeToWaitInSeconds));
+	Concurrency::wait(1000 * Interpreter::Instance()->EvaluateFormulaToFloat(m_timeToWaitInSeconds));
 }
