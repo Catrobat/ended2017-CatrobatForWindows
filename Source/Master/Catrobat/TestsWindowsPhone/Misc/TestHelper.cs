5271068fe431b6611931a9ef4065dc5521378b55
Valentin Rock <CATROBAT\valentin.rock>
2013-06-02 12:57:09 +0200

    Readded TestsWindowsPhone

---
+++ Source/Master/Catrobat/TestsWindowsPhone/Misc/TestHelper.cs
@@ -0,0 +1,49 @@
+﻿using System.IO.IsolatedStorage;
+using Catrobat.Core;
+using Catrobat.Core.Storage;
+using Catrobat.IDEWindowsPhone.Misc.Storage;
+
+namespace Catrobat.TestsWindowsPhone.Misc
+{
+  public static class TestHelper
+  {
+    public static void InitializeAndClearCatrobatContext()
+    {
+      InitializeTests();
+
+      using (var storage = StorageSystem.GetStorage())
+      {
+        storage.DeleteDirectory("");
+      }
+      var catrobatContext = new CatrobatContext();
+      catrobatContext.CurrentProject = null;
+    }
+
+    public static void DeleteFolder(this IsolatedStorageFile iso, string path)
+    {
+      if (!iso.DirectoryExists(path))
+        return;
+      var folders = iso.GetDirectoryNames(path + "/" + "*.*");
+
+      foreach (var folder in folders)
+      {
+        string folderName = path + "/" + folder;
+        DeleteFolder(iso, folderName);
+      }
+
+      foreach (var file in iso.GetFileNames(path + "/" + "*.*"))
+      {
+        iso.DeleteFile(path + "/" + file);
+      }
+
+      if (path != "")
+        iso.DeleteDirectory(path);
+    }
+
+    internal static void InitializeTests()
+    {
+      StorageSystem.SetStorageFactory(new StorageFactoryPhone());
+      ResourceLoader.SetResourceLoaderFactory(new ResourceLoaderFactoryPhone());
+    }
+  }
+}
---
+++ Source/Master/Catrobat/TestsWindowsPhone/SampleData/SampleLoader.cs
@@ -0,0 +1,67 @@
+﻿using Catrobat.Core;
+using Catrobat.Core.Objects;
+using Catrobat.Core.Storage;
+using Catrobat.Core.ZIP;
+using System;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Catrobat.TestsWindowsPhone.SampleData
+{
+  public class SampleLoader
+  {
+    private static string path = "SampleData/SampleProjects/";
+
+    public static Project LoadSampleXML(string sampleName)
+    {
+      String xml = null;
+      using (var resourceLoader = ResourceLoader.CreateResourceLoader())
+      {
+        Stream stream = resourceLoader.OpenResourceStream(ResourceScope.TestsPhone, path + sampleName + ".xml");
+        StreamReader reader = new StreamReader(stream);
+
+        xml = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+        stream.Close();
+        stream.Dispose();
+      }
+      return new Project(xml);
+    }
+
+    public static XDocument LoadSampleXDocument(string sampleName)
+    {
+      String xml = null;
+      using (var resourceLoader = ResourceLoader.CreateResourceLoader())
+      {
+        Stream stream = resourceLoader.OpenResourceStream(ResourceScope.TestsPhone, path + sampleName + ".xml");
+        StreamReader reader = new StreamReader(stream);
+
+        xml = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+        stream.Close();
+        stream.Dispose();
+      }
+      return XDocument.Load(new StringReader(xml));
+    }
+
+    public static CatrobatContext LoadSampleProject(string sampleName, string sampleProjectName)
+    {
+      var catrobatContext = new CatrobatContext();
+      using (var resourceLoader = ResourceLoader.CreateResourceLoader())
+      {
+        Stream stream = resourceLoader.OpenResourceStream(ResourceScope.TestsPhone, path + sampleName + ".xml");
+        CatrobatZip.UnzipCatrobatPackageIntoIsolatedStorage(stream,
+                                                            CatrobatContext.ProjectsPath + "/" + sampleProjectName);
+        stream.Close();
+        stream.Dispose();
+      }
+
+      catrobatContext.SetCurrentProject(sampleProjectName);
+      catrobatContext.CurrentProject.SetProjectName(sampleProjectName);
+
+      return catrobatContext;
+    }
+  }
+}
