d00d59e23984960cc437d63d43d6a91b7517e139
Hannes Hasenauer <CATROBAT\hannes.hasenauer>
2013-06-08 18:16:02 +0200

    ChangeGhostEffectByBrickTEst

--- Source/Master/Catrobat/PlayerWindowsPhone8Test/LookBricks.cpp
+++ Source/Master/Catrobat/PlayerWindowsPhone8Test/LookBricks.cpp
@@ -22,55 +22,88 @@ namespace PlayerWindowsPhone8Test
     TEST_CLASS(LookBricks)
     {
     public:
-        TEST_METHOD(LookBricks_ChangeGhostEffectBrick)
+        TEST_METHOD(LookBricks_ChangeGhostEffectBrick_CheckFor100Percent)
         {
             // TODO: Your test code here
 			string spriteReference = "";
 			Object *object = new Object("TestObject");
 			StartScript *script = new StartScript(spriteReference, object);
-			FormulaTree *formulaTree = new FormulaTree("NUMBER", "10");
-
+			FormulaTree *formulaTree = new FormulaTree("NUMBER", "100");
 			ChangeGhostEffectByBrick *brick = new ChangeGhostEffectByBrick(spriteReference, formulaTree, script);
 
-			float test = object->GetTransparency();
 			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 0.0f));
 			brick->Execute();
-			//Assert::IsTrue(object->GetTransparency() == 1.0f);
+			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 1.0f));
+        }
+
+		TEST_METHOD(LookBricks_ChangeGhostEffectBrick_CheckForOverflow)
+        {
+            // TODO: Your test code here
+			string spriteReference = "";
+			Object *object = new Object("TestObject");
+			StartScript *script = new StartScript(spriteReference, object);
+			FormulaTree *formulaTree = new FormulaTree("NUMBER", "300");
+			ChangeGhostEffectByBrick *brick = new ChangeGhostEffectByBrick(spriteReference, formulaTree, script);
+
+			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 0.0f));
+			brick->Execute();
+			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 1.0f));
+        }
+
+		TEST_METHOD(LookBricks_ChangeGhostEffectBrick_CheckForUnderflow)
+        {
+            // TODO: Your test code here
+			string spriteReference = "";
+			Object *object = new Object("TestObject");
+			StartScript *script = new StartScript(spriteReference, object);
+			FormulaTree *formulaTree = new FormulaTree("NUMBER", "-300");
+			ChangeGhostEffectByBrick *brick = new ChangeGhostEffectByBrick(spriteReference, formulaTree, script);
+
+			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 0.0f));
+			brick->Execute();
+			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 0.0f));
         }
 
 		TEST_METHOD(LookBricks_ChangeSizeByBrickTest)
         {
             // TODO: Your test code here
+			Assert::IsTrue(false);
         }
 
 		TEST_METHOD(LookBricks_CostumeBrick)
         {
             // TODO: Your test code here
+			Assert::IsTrue(false);
         }
 
 		TEST_METHOD(LookBricks_HideBrick)
         {
             // TODO: Your test code here
+			Assert::IsTrue(false);
         }
 
 		TEST_METHOD(LookBricks_NextLookBrick)
         {
             // TODO: Your test code here
+			Assert::IsTrue(false);
         }
 
 		TEST_METHOD(LookBricks_SetGhostEffectBrick)
         {
             // TODO: Your test code here
+			Assert::IsTrue(false);
         }
 
 		TEST_METHOD(LookBricks_SetSizeToBrick)
         {
             // TODO: Your test code here
+			Assert::IsTrue(false);
         }
 
 		TEST_METHOD(LookBricks_ShowBrick)
         {
             // TODO: Your test code here
+			Assert::IsTrue(false);
         }
     };
 }
