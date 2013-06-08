bb0bc478eb48f8c66106a909a1dd00215c267b1e
Hannes Hasenauer <CATROBAT\hannes.hasenauer>
2013-06-08 18:45:09 +0200

    ChangeSizeBrickTests

--- Source/Master/Catrobat/PlayerWindowsPhone8Test/LookBricks.cpp
+++ Source/Master/Catrobat/PlayerWindowsPhone8Test/LookBricks.cpp
@@ -36,6 +36,20 @@ namespace PlayerWindowsPhone8Test
 			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 1.0f));
         }
 
+		TEST_METHOD(LookBricks_ChangeGhostEffectBrick_CheckForZeroPercent)
+        {
+            // TODO: Your test code here
+			string spriteReference = "";
+			Object *object = new Object("TestObject");
+			StartScript *script = new StartScript(spriteReference, object);
+			FormulaTree *formulaTree = new FormulaTree("NUMBER", "0");
+			ChangeGhostEffectByBrick *brick = new ChangeGhostEffectByBrick(spriteReference, formulaTree, script);
+
+			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 0.0f));
+			brick->Execute();
+			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 0.0f));
+        }
+
 		TEST_METHOD(LookBricks_ChangeGhostEffectBrick_CheckForOverflow)
         {
             // TODO: Your test code here
@@ -48,6 +62,44 @@ namespace PlayerWindowsPhone8Test
 			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 0.0f));
 			brick->Execute();
 			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 1.0f));
+        }
+
+		TEST_METHOD(LookBricks_ChangeGhostEffectBrick_CheckFor50Percent)
+        {
+            // TODO: Your test code here
+			string spriteReference = "";
+			Object *object = new Object("TestObject");
+			StartScript *script = new StartScript(spriteReference, object);
+			FormulaTree *formulaTree = new FormulaTree("NUMBER", "50");
+			ChangeGhostEffectByBrick *brick = new ChangeGhostEffectByBrick(spriteReference, formulaTree, script);
+
+			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 0.0f));
+			brick->Execute();
+			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 0.5f));
+        }
+
+		TEST_METHOD(LookBricks_ChangeGhostEffectBrick_CheckForVariousChanges)
+        {
+            // TODO: Your test code here
+			string spriteReference = "";
+			Object *object = new Object("TestObject");
+			StartScript *script = new StartScript(spriteReference, object);
+			FormulaTree *formulaTree = new FormulaTree("NUMBER", "30");
+			ChangeGhostEffectByBrick *brick = new ChangeGhostEffectByBrick(spriteReference, formulaTree, script);
+
+			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 0.0f));
+			brick->Execute();
+			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 0.3f));
+
+			formulaTree = new FormulaTree("NUMBER", "40");
+			brick = new ChangeGhostEffectByBrick(spriteReference, formulaTree, script);
+			brick->Execute();
+			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 0.7f));
+
+			formulaTree = new FormulaTree("NUMBER", "-10");
+			brick = new ChangeGhostEffectByBrick(spriteReference, formulaTree, script);
+			brick->Execute();
+			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 0.6f));
         }
 
 		TEST_METHOD(LookBricks_ChangeGhostEffectBrick_CheckForUnderflow)
@@ -64,10 +116,94 @@ namespace PlayerWindowsPhone8Test
 			Assert::IsTrue(TestHelper::isEqual(object->GetTransparency(), 0.0f));
         }
 
-		TEST_METHOD(LookBricks_ChangeSizeByBrickTest)
+		TEST_METHOD(LookBricks_ChangeSizeByBrickTest_CheckForZero)
         {
             // TODO: Your test code here
-			Assert::IsTrue(false);
+			string spriteReference = "";
+			Object *object = new Object("TestObject");
+			StartScript *script = new StartScript(spriteReference, object);
+			FormulaTree *formulaTree = new FormulaTree("NUMBER", "-1");
+			ChangeSizeByBrick *brick = new ChangeSizeByBrick(spriteReference, formulaTree, script);
+
+			Assert::IsTrue(TestHelper::isEqual(object->GetScale(), 1.0f));
+			brick->Execute();
+			Assert::IsTrue(TestHelper::isEqual(object->GetScale(), 0.0f));
+        }
+
+		TEST_METHOD(LookBricks_ChangeSizeByBrickTest_CheckForUnderflow)
+        {
+            // TODO: Your test code here
+			string spriteReference = "";
+			Object *object = new Object("TestObject");
+			StartScript *script = new StartScript(spriteReference, object);
+			FormulaTree *formulaTree = new FormulaTree("NUMBER", "-5");
+			ChangeSizeByBrick *brick = new ChangeSizeByBrick(spriteReference, formulaTree, script);
+
+			Assert::IsTrue(TestHelper::isEqual(object->GetScale(), 1.0f));
+			brick->Execute();
+			Assert::IsTrue(TestHelper::isEqual(object->GetScale(), 0.0f));
+        }
+
+		TEST_METHOD(LookBricks_ChangeSizeByBrickTest_CheckFor8)
+        {
+            // TODO: Your test code here
+			string spriteReference = "";
+			Object *object = new Object("TestObject");
+			StartScript *script = new StartScript(spriteReference, object);
+			FormulaTree *formulaTree = new FormulaTree("NUMBER", "7");
+			ChangeSizeByBrick *brick = new ChangeSizeByBrick(spriteReference, formulaTree, script);
+
+			Assert::IsTrue(TestHelper::isEqual(object->GetScale(), 1.0f));
+			brick->Execute();
+			Assert::IsTrue(TestHelper::isEqual(object->GetScale(), 8.0f));
+        }
+
+		TEST_METHOD(LookBricks_ChangeSizeByBrickTest_CheckFor03)
+        {
+            // TODO: Your test code here
+			string spriteReference = "";
+			Object *object = new Object("TestObject");
+			StartScript *script = new StartScript(spriteReference, object);
+			FormulaTree *formulaTree = new FormulaTree("NUMBER", "-0.7");
+			ChangeSizeByBrick *brick = new ChangeSizeByBrick(spriteReference, formulaTree, script);
+
+			Assert::IsTrue(TestHelper::isEqual(object->GetScale(), 1.0f));
+			brick->Execute();
+			Assert::IsTrue(TestHelper::isEqual(object->GetScale(), 0.3f));
+        }
+
+		TEST_METHOD(LookBricks_ChangeSizeByBrickTest_CheckForVariousChanges)
+        {
+            // TODO: Your test code here
+			string spriteReference = "";
+			Object *object = new Object("TestObject");
+			StartScript *script = new StartScript(spriteReference, object);
+			FormulaTree *formulaTree = new FormulaTree("NUMBER", "0");
+			ChangeSizeByBrick *brick = new ChangeSizeByBrick(spriteReference, formulaTree, script);
+
+			Assert::IsTrue(TestHelper::isEqual(object->GetScale(), 1.0f));
+			brick->Execute();
+			Assert::IsTrue(TestHelper::isEqual(object->GetScale(), 1.0f));
+
+			formulaTree = new FormulaTree("NUMBER", "-5");
+			brick = new ChangeSizeByBrick(spriteReference, formulaTree, script);
+			brick->Execute();
+			Assert::IsTrue(TestHelper::isEqual(object->GetScale(), 0.0f));
+
+			formulaTree = new FormulaTree("NUMBER", "3");
+			brick = new ChangeSizeByBrick(spriteReference, formulaTree, script);
+			brick->Execute();
+			Assert::IsTrue(TestHelper::isEqual(object->GetScale(), 3.0f));
+
+			formulaTree = new FormulaTree("NUMBER", "-2.8");
+			brick = new ChangeSizeByBrick(spriteReference, formulaTree, script);
+			brick->Execute();
+			Assert::IsTrue(TestHelper::isEqual(object->GetScale(), 0.2f));
+
+			formulaTree = new FormulaTree("NUMBER", "1.9");
+			brick = new ChangeSizeByBrick(spriteReference, formulaTree, script);
+			brick->Execute();
+			Assert::IsTrue(TestHelper::isEqual(object->GetScale(), 2.1f));
         }
 
 		TEST_METHOD(LookBricks_CostumeBrick)
