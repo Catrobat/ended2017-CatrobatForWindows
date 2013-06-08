602d31798faa6f76ee62292c66fb5e0bd1923c07
Maximilian Sachs <CATROBAT\maximilian.sachs>
2013-06-08 18:49:46 +0200

    Movement Tests.

--- Source/Master/Catrobat/PlayerWindowsPhone8Test/MovementBricks.cpp
+++ Source/Master/Catrobat/PlayerWindowsPhone8Test/MovementBricks.cpp
@@ -12,6 +12,7 @@
 #include "TurnLeftBrick.h"
 #include "TurnRightBrick.h"
 #include "FormulaTree.h"
+#include "TestHelper.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -38,6 +39,64 @@ namespace PlayerWindowsPhone8Test
 			object->GetPosition(x, y);
 
 			Assert::AreEqual(x, old_x + 42);
+
+			brick->Execute();
+
+			object->GetPosition(x, y);
+
+			Assert::AreEqual(x, old_x + 84);
+        }
+
+		TEST_METHOD(MovementBricks_ChangeXByBrick_Negative)
+        {
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree = new FormulaTree("NUMBER", "-42");
+			Brick* brick = new ChangeXByBrick("", tree, script);
+			
+			float old_x = 0;
+			float old_y = 0;
+			object->SetPosition(old_x, old_y);
+
+			brick->Execute();
+
+			float x = 0;
+			float y = 0;
+			object->GetPosition(x, y);
+
+			Assert::AreEqual(x, old_x - 42);
+
+			brick->Execute();
+
+			object->GetPosition(x, y);
+
+			Assert::AreEqual(x, old_x - 84);
+        }
+
+		TEST_METHOD(MovementBricks_ChangeXByBrick_zero)
+        {
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree = new FormulaTree("NUMBER", "0");
+			Brick* brick = new ChangeXByBrick("", tree, script);
+			
+			float old_x = 0;
+			float old_y = 0;
+			object->SetPosition(old_x, old_y);
+
+			brick->Execute();
+
+			float x = 0;
+			float y = 0;
+			object->GetPosition(x, y);
+
+			Assert::AreEqual(x, old_x);
+
+			brick->Execute();
+
+			object->GetPosition(x, y);
+
+			Assert::AreEqual(x, old_x);
         }
 
 		TEST_METHOD(MovementBricks_ChangeYByBrick)
@@ -58,24 +117,186 @@ namespace PlayerWindowsPhone8Test
 			object->GetPosition(x, y);
 
 			Assert::AreEqual(y, old_y + 42);
+
+			brick->Execute();
+
+			object->GetPosition(x, y);
+
+			Assert::AreEqual(y, old_y + 84);
         }
 
-		TEST_METHOD(MovementBricks_GlideToBrick)
+		TEST_METHOD(MovementBricks_ChangeYByBrick_Negative)
         {
-            // TODO: Your test code here
-			Assert::IsTrue(false);
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree = new FormulaTree("NUMBER", "-42");
+			Brick* brick = new ChangeYByBrick("", tree, script);
+			
+			float old_x = 0;
+			float old_y = 0;
+			object->SetPosition(old_x, old_y);
+
+			brick->Execute();
+
+			float x = 0;
+			float y = 0;
+			object->GetPosition(x, y);
+
+			Assert::AreEqual(y, old_y - 42);
+
+			brick->Execute();
+
+			object->GetPosition(x, y);
+
+			Assert::AreEqual(y, old_y - 84);
         }
 
-		TEST_METHOD(MovementBricks_PlaceAtBrick)
+		TEST_METHOD(MovementBricks_ChangeYByBrick_Zero)
         {
-            // TODO: Your test code here
-			Assert::IsTrue(false);
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree = new FormulaTree("NUMBER", "0");
+			Brick* brick = new ChangeYByBrick("", tree, script);
+			
+			float old_x = 0;
+			float old_y = 0;
+			object->SetPosition(old_x, old_y);
+
+			brick->Execute();
+
+			float x = 0;
+			float y = 0;
+			object->GetPosition(x, y);
+
+			Assert::AreEqual(y, old_y);
+
+			brick->Execute();
+
+			object->GetPosition(x, y);
+
+			Assert::AreEqual(y, old_y);
         }
 
-		TEST_METHOD(MovementBricks_PointToBrick)
+		TEST_METHOD(MovementBricks_GlideToBrick_Variance1)
         {
-            // TODO: Your test code here
-			Assert::IsTrue(false);
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree1 = new FormulaTree("NUMBER", "42");
+			FormulaTree* tree2 = new FormulaTree("NUMBER", "23");
+			FormulaTree* tree3 = new FormulaTree("NUMBER", "3000");
+			Brick* brick = new GlideToBrick("", tree1, tree2, tree3, script);
+			object->SetPosition(0, 0);
+
+			brick->Execute();
+
+			float x = 0;
+			float y = 0;
+			object->GetPosition(x, y);
+
+			Assert::IsTrue(TestHelper::isEqual(x, 42.0f));
+			Assert::IsTrue(TestHelper::isEqual(y, 23.0f));
+
+			// TODO: Measure Time.
+        }
+
+		TEST_METHOD(MovementBricks_GlideToBrick_Variance2)
+        {
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree1 = new FormulaTree("NUMBER", "123");
+			FormulaTree* tree2 = new FormulaTree("NUMBER", "456");
+			FormulaTree* tree3 = new FormulaTree("NUMBER", "5000");
+			Brick* brick = new GlideToBrick("", tree1, tree2, tree3, script);
+			object->SetPosition(0, 0);
+
+			brick->Execute();
+
+			float x = 0;
+			float y = 0;
+			object->GetPosition(x, y);
+
+			Assert::IsTrue(TestHelper::isEqual(x, 123.0f));
+			Assert::IsTrue(TestHelper::isEqual(y, 456.0f));
+
+			// TODO: Measure Time.
+        }
+
+		TEST_METHOD(MovementBricks_PlaceAtBrick_Variance1)
+        {
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree1 = new FormulaTree("NUMBER", "23");
+			FormulaTree* tree2 = new FormulaTree("NUMBER", "32");
+			Brick* brick = new PlaceAtBrick("", tree1, tree2, script);
+
+			brick->Execute();
+
+			float x = 0;
+			float y = 0;
+			object->GetPosition(x, y);
+
+			Assert::AreEqual(x, 23.0f);
+			Assert::AreEqual(y, 32.0f);
+        }
+
+		TEST_METHOD(MovementBricks_PlaceAtBrick_Variance2)
+        {
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree1 = new FormulaTree("NUMBER", "23");
+			FormulaTree* tree2 = new FormulaTree("NUMBER", "32");
+			Brick* brick = new PlaceAtBrick("", tree1, tree2, script);
+
+			brick->Execute();
+
+			float x = 0;
+			float y = 0;
+			object->GetPosition(x, y);
+
+			Assert::AreEqual(x, 23.0f);
+			Assert::AreEqual(y, 32.0f);
+        }
+
+		TEST_METHOD(MovementBricks_PointToBrick_Variance1)
+        {
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree = new FormulaTree("NUMBER", "42");
+			Brick* brick = new PointToBrick("", tree, script);
+			object->SetRotation(0.0f);
+
+			brick->Execute();
+
+			float r = object->GetRotation();
+
+			Assert::AreEqual(r, 42.0f);
+
+			brick->Execute();
+
+			r = object->GetRotation();
+
+			Assert::AreEqual(r, 42.0f);
+        }
+
+		TEST_METHOD(MovementBricks_PointToBrick_Variance2)
+        {
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree = new FormulaTree("NUMBER", "23");
+			Brick* brick = new PointToBrick("", tree, script);
+			object->SetRotation(0.0f);
+
+			brick->Execute();
+
+			float r = object->GetRotation();
+
+			Assert::AreEqual(r, 23.0f);
+
+			brick->Execute();
+
+			r = object->GetRotation();
+
+			Assert::AreEqual(r, 23.0f);
         }
 
 		TEST_METHOD(MovementBricks_SetXBrick)
@@ -94,7 +315,39 @@ namespace PlayerWindowsPhone8Test
 			Assert::AreEqual(x, 42.0f);
         }
 
-		TEST_METHOD(MovementBricks_SetYByBrick)
+		TEST_METHOD(MovementBricks_SetXBrick_Negative)
+        {
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree = new FormulaTree("NUMBER", "-42");
+			Brick* brick = new SetXBrick("", tree, script);
+
+			brick->Execute();
+
+			float x = 0;
+			float y = 0;
+			object->GetPosition(x, y);
+
+			Assert::AreEqual(x, -42.0f);
+        }
+
+		TEST_METHOD(MovementBricks_SetXBrick_Zero)
+        {
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree = new FormulaTree("NUMBER", "0");
+			Brick* brick = new SetXBrick("", tree, script);
+
+			brick->Execute();
+
+			float x = 0;
+			float y = 0;
+			object->GetPosition(x, y);
+
+			Assert::AreEqual(x, 0.0f);
+        }
+
+		TEST_METHOD(MovementBricks_SetYBrick)
         {
 			Object* object = new Object("");
 			Script* script = new StartScript("", object);
@@ -110,16 +363,162 @@ namespace PlayerWindowsPhone8Test
 			Assert::AreEqual(y, 42.0f);
         }
 
+		TEST_METHOD(MovementBricks_SetYBrick_Negative)
+        {
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree = new FormulaTree("NUMBER", "-42");
+			Brick* brick = new SetYBrick("", tree, script);
+
+			brick->Execute();
+
+			float x = 0;
+			float y = 0;
+			object->GetPosition(x, y);
+
+			Assert::AreEqual(y, -42.0f);
+        }
+
+		TEST_METHOD(MovementBricks_SetYBrick_Zero)
+        {
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree = new FormulaTree("NUMBER", "0");
+			Brick* brick = new SetYBrick("", tree, script);
+
+			brick->Execute();
+
+			float x = 0;
+			float y = 0;
+			object->GetPosition(x, y);
+
+			Assert::AreEqual(y, 0.0f);
+        }
+
 		TEST_METHOD(MovementBricks_TurnLeftBrick)
         {
-            // TODO: Your test code here
-			Assert::IsTrue(false);
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree = new FormulaTree("NUMBER", "42");
+			Brick* brick = new TurnLeftBrick("", tree, script);
+			object->SetRotation(0.0f);
+
+			brick->Execute();
+
+			float r = object->GetRotation();
+
+			Assert::AreEqual(r, -42.0f);
+
+			brick->Execute();
+
+			r = object->GetRotation();
+
+			Assert::AreEqual(r, -84.0f);
+        }
+
+		TEST_METHOD(MovementBricks_TurnLeftBrick_Zero)
+        {
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree = new FormulaTree("NUMBER", "0");
+			Brick* brick = new TurnLeftBrick("", tree, script);
+			object->SetRotation(23.0f);
+
+			brick->Execute();
+
+			float r = object->GetRotation();
+
+			Assert::AreEqual(r, 23.0f);
+
+			brick->Execute();
+
+			r = object->GetRotation();
+
+			Assert::AreEqual(r, 23.0f);
+        }
+
+		TEST_METHOD(MovementBricks_TurnLeftBrick_Negative)
+        {
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree = new FormulaTree("NUMBER", "-42");
+			Brick* brick = new TurnLeftBrick("", tree, script);
+			object->SetRotation(0.0f);
+
+			brick->Execute();
+
+			float r = object->GetRotation();
+
+			Assert::AreEqual(r, 42.0f);
+
+			brick->Execute();
+
+			r = object->GetRotation();
+
+			Assert::AreEqual(r, 84.0f);
         }
 
 		TEST_METHOD(MovementBricks_TurnRightBrick)
         {
-            // TODO: Your test code here
-			Assert::IsTrue(false);
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree = new FormulaTree("NUMBER", "42");
+			Brick* brick = new TurnRightBrick("", tree, script);
+			object->SetRotation(0.0f);
+
+			brick->Execute();
+
+			float r = object->GetRotation();
+
+			Assert::AreEqual(r, 42.0f);
+
+			brick->Execute();
+
+			r = object->GetRotation();
+
+			Assert::AreEqual(r, 84.0f);
+        }
+
+		TEST_METHOD(MovementBricks_TurnRightBrick_Zero)
+        {
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree = new FormulaTree("NUMBER", "0");
+			Brick* brick = new TurnRightBrick("", tree, script);
+			object->SetRotation(23.0f);
+
+			brick->Execute();
+
+			float r = object->GetRotation();
+
+			Assert::AreEqual(r, 23.0f);
+
+			brick->Execute();
+
+			r = object->GetRotation();
+
+			Assert::AreEqual(r, 23.0f);
+        }
+
+		TEST_METHOD(MovementBricks_TurnRightBrick_Negative)
+        {
+			Object* object = new Object("");
+			Script* script = new StartScript("", object);
+			FormulaTree* tree = new FormulaTree("NUMBER", "-42");
+			Brick* brick = new TurnRightBrick("", tree, script);
+			object->SetRotation(0.0f);
+
+			brick->Execute();
+
+			float r = object->GetRotation();
+
+			Assert::AreEqual(r, -42.0f);
+
+			brick->Execute();
+
+			r = object->GetRotation();
+
+			Assert::AreEqual(r, -84.0f);
         }
     };
 }
