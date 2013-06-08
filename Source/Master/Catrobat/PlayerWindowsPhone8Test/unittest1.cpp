15a46c81a5563c7e84219fad926b53d7d926bab5
Maximilian Sachs <CATROBAT\maximilian.sachs>
2013-06-08 11:13:09 +0200

    Testproject.

---
+++ Source/Master/Catrobat/PlayerWindowsPhone8Test/unittest1.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "ChangeGhostEffectByBrick.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PlayerWindowsPhone8Test
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+        TEST_METHOD(TestMethod1)
+        {
+            // TODO: Your test code here
+        }
+    };
+}
