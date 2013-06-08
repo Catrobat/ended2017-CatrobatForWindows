9b2626c373117c0113cb6f0221a280ff13fbc985
Maximilian Sachs <CATROBAT\maximilian.sachs>
2013-06-08 17:22:59 +0200

    SoundManager Flag.

--- Source/Master/Catrobat/PlayerWindowsPhone8/PlayerWindowsPhone8Component/SoundManager.cpp
+++ Source/Master/Catrobat/PlayerWindowsPhone8/PlayerWindowsPhone8Component/SoundManager.cpp
@@ -10,27 +10,34 @@ SoundManager *SoundManager::__instance = NULL;
 
 SoundManager *SoundManager::Instance()
 {
-	//if (!__instance)
-	//	__instance = new SoundManager();
-	//return __instance;
+#ifndef UNITTEST
+	if (!__instance)
+		__instance = new SoundManager();
+	return __instance;
+#else
 	return NULL;
+#endif
 }
 
 SoundManager::SoundManager(void)
 {
-	//channel = 0;
-	//extradriverdata = 0;
-	//m_sounds = new map<string, Sound*>();
+#ifndef UNITTEST
+	channel = 0;
+	extradriverdata = 0;
+	m_sounds = new map<string, Sound*>();
+#endif
 }
 
 void SoundManager::Initialize()
 {
-    //status = FMOD::System_Create(&system);
-    //status = system->getVersion(&version);
-    //FMOD_SPEAKERMODE speakerMode = FMOD_SPEAKERMODE_STEREO;
-    //status = system->getDriverCaps(0, NULL, NULL, &speakerMode);
-    //status = system->setSpeakerMode(speakerMode);
-    //status = system->init(32, FMOD_INIT_NORMAL, extradriverdata);
+#ifndef UNITTEST
+    status = FMOD::System_Create(&system);
+    status = system->getVersion(&version);
+    FMOD_SPEAKERMODE speakerMode = FMOD_SPEAKERMODE_STEREO;
+    status = system->getDriverCaps(0, NULL, NULL, &speakerMode);
+    status = system->setSpeakerMode(speakerMode);
+    status = system->init(32, FMOD_INIT_NORMAL, extradriverdata);
+#endif
 }
 
 SoundManager::~SoundManager(void)
@@ -39,18 +46,21 @@ SoundManager::~SoundManager(void)
 
 Sound* SoundManager::CreateOrGetSound(std::string filename)
 {
-	//map<string, Sound*>::iterator currentSound;
-	//currentSound = m_sounds->find(filename);
-	//
-	//if (currentSound == m_sounds->end())
-	//{
-	//	Sound* result = new Sound(system, channel);
-	//	result->Load(filename);
-	//	return result;
-	//}
-	//else
-	//{
-	//	return currentSound->second;
-	//}
+#ifndef UNITTEST
+	map<string, Sound*>::iterator currentSound;
+	currentSound = m_sounds->find(filename);
+	
+	if (currentSound == m_sounds->end())
+	{
+		Sound* result = new Sound(system, channel);
+		result->Load(filename);
+		return result;
+	}
+	else
+	{
+		return currentSound->second;
+	}
+#else
 	return NULL;
+#endif
 }
--- Source/Master/Catrobat/PlayerWindowsPhone8Test/pch.h
+++ Source/Master/Catrobat/PlayerWindowsPhone8Test/pch.h
@@ -1,4 +1,6 @@
-﻿#pragma once
+﻿#define UNITTEST
+
+#pragma once
 
 #include <wrl/client.h>
 #include <d3d11_1.h>
