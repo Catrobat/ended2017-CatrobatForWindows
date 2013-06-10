14b9f9b5c578e1c6d819d7003161dccebf2e5db8
Hannes Hasenauer <CATROBAT\hannes.hasenauer>
2013-06-10 11:55:44 +0200

    ControlBricks Testclass setup

---
+++ Source/Master/Catrobat/PlayerWindowsPhone8Test/ControlBricks.cpp
@@ -0,0 +1,53 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "TestHelper.h"
+
+#include "Object.h"
+#include "FormulaTree.h"
+#include "StartScript.h"
+
+#include "BroadcastBrick.h"
+#include "ForeverBrick.h"
+#include "IfBrick.h"
+#include "RepeatBrick.h"
+#include "WaitBrick.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PlayerWindowsPhone8Test
+{
+	TEST_CLASS(ControlBricks)
+	{
+	public:
+		
+		TEST_METHOD(ControlBricks_BroadcastBrick)
+		{
+			// TODO: Your test code here
+			Assert::IsTrue(false);
+		}
+
+		TEST_METHOD(ControlBricks_ForeverBrick)
+		{
+			// TODO: Your test code here
+			Assert::IsTrue(false);
+		}
+
+		TEST_METHOD(ControlBricks_IfBrick)
+		{
+			// TODO: Your test code here
+			Assert::IsTrue(false);
+		}
+
+		TEST_METHOD(ControlBricks_RepeatBrick)
+		{
+			// TODO: Your test code here
+			Assert::IsTrue(false);
+		}
+
+		TEST_METHOD(ControlBricks_WaitBrick)
+		{
+			// TODO: Your test code here
+			Assert::IsTrue(false);
+		}
+	};
+}
