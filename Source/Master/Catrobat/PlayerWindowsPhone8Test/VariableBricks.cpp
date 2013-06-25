c3303c55f01d0bca65b58096d689d31aff143953
Hannes Hasenauer <CATROBAT\hannes.hasenauer>
2013-06-25 16:06:28 +0200

    variable brick tests

--- Source/Master/Catrobat/PlayerWindowsPhone8Test/VariableBricks.cpp
+++ Source/Master/Catrobat/PlayerWindowsPhone8Test/VariableBricks.cpp
@@ -18,23 +18,76 @@ namespace PlayerWindowsPhone8Test
 	{
 	public:
 		
-		TEST_METHOD(VariableBricks_ChangeVariableBrick)
+		TEST_METHOD(VariableBricks_SetVariableBrick_SimpleCheck)
 		{
             string variableName = "testVariable";
-            UserVariable *variable = new UserVariable(variableName, "10");
+            UserVariable *variable = new UserVariable(variableName, "5");
             FormulaTree *formulaTree = new FormulaTree("USER_VARIABLE", variableName);
 			string spriteReference = "";
 			Object *object = new Object("TestObject"); 
 
 			StartScript *script = new StartScript(spriteReference, object);
+            formulaTree = new FormulaTree("NUMBER", "10");
             SetVariableBrick *brick = new SetVariableBrick(spriteReference, formulaTree, script);
-            Assert::IsTrue(false);
+            brick->SetVariable(variable);
+
+            Assert::AreEqual(atoi(variable->Value().c_str()), 5);
+            brick->Execute();
+            Assert::AreEqual(atoi(variable->Value().c_str()), 10);
 		}
 
-        TEST_METHOD(VariableBricks_SetVariableBrick)
+        TEST_METHOD(VariableBricks_SetVariableBrick_NegativCheck)
 		{
-			// TODO: Your test code here
-            Assert::IsTrue(false);
+            string variableName = "testVariable";
+            UserVariable *variable = new UserVariable(variableName, "5");
+            FormulaTree *formulaTree = new FormulaTree("USER_VARIABLE", variableName);
+			string spriteReference = "";
+			Object *object = new Object("TestObject"); 
+
+			StartScript *script = new StartScript(spriteReference, object);
+            formulaTree = new FormulaTree("NUMBER", "-20");
+            SetVariableBrick *brick = new SetVariableBrick(spriteReference, formulaTree, script);
+            brick->SetVariable(variable);
+
+            Assert::AreEqual(atoi(variable->Value().c_str()), 5);
+            brick->Execute();
+            Assert::AreEqual(atoi(variable->Value().c_str()), -20);
+		}
+
+        TEST_METHOD(VariableBricks_ChangeVariableBrick_SimpleCheck)
+		{
+            string variableName = "testVariable";
+            UserVariable *variable = new UserVariable(variableName, "5");
+            FormulaTree *formulaTree = new FormulaTree("USER_VARIABLE", variableName);
+			string spriteReference = "";
+			Object *object = new Object("TestObject"); 
+
+			StartScript *script = new StartScript(spriteReference, object);
+            formulaTree = new FormulaTree("NUMBER", "10");
+            ChangeVariableBrick *brick = new ChangeVariableBrick(spriteReference, formulaTree, script);
+            brick->SetVariable(variable);
+
+            Assert::AreEqual(atoi(variable->Value().c_str()), 5);
+            brick->Execute();
+            Assert::AreEqual(atoi(variable->Value().c_str()), 15);
+		}
+
+        TEST_METHOD(VariableBricks_ChangeVariableBrick_NegativCheck)
+		{
+			string variableName = "testVariable";
+            UserVariable *variable = new UserVariable(variableName, "5");
+            FormulaTree *formulaTree = new FormulaTree("USER_VARIABLE", variableName);
+			string spriteReference = "";
+			Object *object = new Object("TestObject"); 
+
+			StartScript *script = new StartScript(spriteReference, object);
+            formulaTree = new FormulaTree("NUMBER", "-20");
+            ChangeVariableBrick *brick = new ChangeVariableBrick(spriteReference, formulaTree, script);
+            brick->SetVariable(variable);
+
+            Assert::AreEqual(atoi(variable->Value().c_str()), 5);
+            brick->Execute();
+            Assert::AreEqual(atoi(variable->Value().c_str()), -15);
 		}
 	};
 }
