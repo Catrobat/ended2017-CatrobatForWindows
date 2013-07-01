48e8ddd59f7893bcfd96a3f282be9fab05c9c8b2
Hannes Hasenauer <CATROBAT\hannes.hasenauer>
2013-07-01 14:19:32 +0200

    error handling and testing xmlparser

--- Source/Master/Catrobat/PlayerWindowsPhone8Test/XMLParserTests.cpp
+++ Source/Master/Catrobat/PlayerWindowsPhone8Test/XMLParserTests.cpp
@@ -15,11 +15,22 @@ namespace PlayerWindowsPhone8Test
         TEST_METHOD(XMLParserTests_FileNotFound)
 		{
             XMLParser *parser = new XMLParser();
-            parser->loadXML("nonExistingProject.xml");
-            Assert::IsTrue(false);
+            bool success = parser->loadXML("nonExistingProject.xml");
+            Assert::IsFalse(success);
 		}
 
 		TEST_METHOD(XMLParserTests_Header)
+		{
+            XMLParser *parser = new XMLParser();
+            bool success = parser->loadXML("TestFiles/headerTest.xml");
+			Assert::IsTrue(success);
+
+            Project *project = parser->getProject();
+            Assert::AreEqual(project->ScreenHeight(), 1184);
+            Assert::AreEqual(project->ScreenWidth(), 768);
+		}
+
+		TEST_METHOD(XMLParserTests_SimpleObjectList)
 		{
             XMLParser *parser = new XMLParser();
             parser->loadXML("TestFiles/headerTest.xml");
