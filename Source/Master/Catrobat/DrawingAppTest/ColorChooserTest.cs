c832f788cdf93d4b2240c1b4e8d3751e38cb8828
Max Schafzahl <CATROBAT\max.schafzahl>
2013-07-16 18:41:33 +0200

    Paint: Added Test Project and InkPresenter

---
+++ Source/Master/Catrobat/DrawingAppTest/ColorChooserTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace DrawingAppTest
+{
+    [TestClass]
+    public class ColorChooserTest
+    {
+        [TestMethod]
+        [Description("A first unit test.")]
+        public void ColorSelected()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
