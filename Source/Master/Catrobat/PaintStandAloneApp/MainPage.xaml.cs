bf96844184f72db8e7a5af47ea455afe239e968a
Valentin Rock <CATROBAT\valentin.rock>
2013-07-18 23:30:59 +0200

    Finished PaintStandAloneApp

--- Source/Master/Catrobat/PaintStandAloneApp/MainPage.xaml.cs
+++ Source/Master/Catrobat/PaintStandAloneApp/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Phone.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Catrobat.Paint;
+using Microsoft.Phone.Controls;
 
 namespace Catrobat.PaintStandAloneApp
 {
@@ -8,25 +12,13 @@ namespace Catrobat.PaintStandAloneApp
         public MainPage()
         {
             InitializeComponent();
-
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
+            Loaded += OnLoaded;
         }
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
-
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            PaintLauncher.CurrentImage = new BitmapImage(new Uri("/Content/TestImage.png", UriKind.Relative));
+            PaintLauncher.Launche();
+        }
     }
 }
