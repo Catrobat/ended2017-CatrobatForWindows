48093967efdc7bfd6f8c47e5cabc7fd9c17276e2
Valentin Rock <CATROBAT\valentin.rock>
2013-08-02 12:21:55 +0200

    Added MultilingualAppToolkit to Paint

---
+++ Source/Master/Catrobat/Paint/Resources/LocalizedStrings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using Catrobat.Paint.Misc;
+
+namespace Catrobat.Paint.Resources
+{
+  public class LocalizedStrings : INotifyPropertyChanged
+  {
+      private static readonly AppResources ResourcesField = new AppResources();
+    public AppResources Resources { get { return ResourcesField; } }
+
+    public void Reset()
+    {
+        RaisePropertyChanged(() => Resources);
+    }
+
+    #region INotifyPropertyChanged region
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void RaisePropertyChanged<T>(Expression<Func<T>> selector)
+    {
+      if (PropertyChanged != null)
+      {
+          PropertyChanged(this, new PropertyChangedEventArgs(PropertyNameHelper.GetPropertyNameFromExpression(selector)));
+      }
+    }
+    #endregion
+  }
+}
