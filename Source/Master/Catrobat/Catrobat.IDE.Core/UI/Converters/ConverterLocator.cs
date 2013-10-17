42a072af1777ea100bfc9ff7ff46b8747250859f
Valentin Rock <CATROBAT\valentin.rock>
2013-10-17 17:37:17 +0200

    Moved some tests to Core, added PortableConverter stuff ***NO_CI***

---
+++ Source/Master/Catrobat/Catrobat.IDE.Core/UI/Converters/ConverterLocator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catrobat.IDE.Core.UI.Converters
+{
+    public class ConverterLocator
+    {
+        public List<IValueConverter> Converter = new List<IValueConverter>();
+ 
+        //public void RegisterConverter
+
+    }
+}
