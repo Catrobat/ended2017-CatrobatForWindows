6179f80d0e7f450d3682e7b58a0c5085ec092b11
Valentin Rock <CATROBAT\valentin.rock>
2013-11-23 22:17:36 +0100

    Lots of UI changes for Win8 (SpriteEditorPresenter) ***NO_CI***

--- Source/Master/Catrobat/Catrobat.IDE.Store/Views/Editor/Sprites/SpritesPresenter.xaml.cs
+++ Source/Master/Catrobat/Catrobat.IDE.Store/Views/Editor/Sprites/SpritesPresenter.xaml.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
+using Windows.UI;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Catrobat.IDE.Core.CatrobatObjects;
+using Catrobat.IDE.Core.CatrobatObjects.Costumes;
+using Catrobat.IDE.Core.CatrobatObjects.Sounds;
 using Catrobat.IDE.Core.Services;
 using Catrobat.IDE.Core.Utilities.Helpers;
 using Catrobat.IDE.Core.ViewModel.Editor.Costumes;
 using Catrobat.IDE.Core.ViewModel.Editor.Scripts;
+using Catrobat.IDE.Core.ViewModel.Editor.Sounds;
 using Catrobat.IDE.Core.ViewModel.Editor.Sprites;
 using Windows.UI.Xaml.Controls;
+using Catrobat.IDE.Store.Views.Editor.Sounds;
 
 namespace Catrobat.IDE.Store.Views.Editor.Sprites
 {
@@ -14,39 +25,53 @@ namespace Catrobat.IDE.Store.Views.Editor.Sprites
     {
         private readonly SpritesViewModel _spritesViewModel = ServiceLocator.GetInstance<SpritesViewModel>();
         private readonly SpriteEditorViewModel _spriteEditorViewModel = ServiceLocator.GetInstance<SpriteEditorViewModel>();
+        private readonly FrameworkElement _appBarObjects;
+        private readonly FrameworkElement _appBarActions;
+        private readonly FrameworkElement _appBarLooks;
+        private readonly FrameworkElement _appBarSounds;
 
         public SpritesPresenter()
         {
             this.InitializeComponent();
+
+            if (ItemsControlAppBars.Items != null)
+            {
+                _appBarObjects = (FrameworkElement) ItemsControlAppBars.Items[0];
+                _appBarActions = (FrameworkElement)ItemsControlAppBars.Items[1];
+                _appBarLooks = (FrameworkElement)ItemsControlAppBars.Items[2];
+                _appBarSounds = (FrameworkElement)ItemsControlAppBars.Items[3];
+            }
+
             _spritesViewModel.PropertyChanged += ViewModelOnPropertyChanged;
         }
 
         private void ViewModelOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
         {
-            if (propertyChangedEventArgs.PropertyName ==
-                PropertyHelper.GetPropertyName(() => _spritesViewModel.SelectedSprite))
-                AppBarBottomn.IsOpen = _spritesViewModel.SelectedSprite != null;
+            //if (propertyChangedEventArgs.PropertyName ==
+            //    PropertyHelper.GetPropertyName(() => _spritesViewModel.SelectedSprite))
+            //{
+            //    AppBarBottomn.IsOpen = _spritesViewModel.SelectedSprite != null;
+            //}
         }
 
-        private void FlyoutNew_OnOpen(object sender, object e)
+        private void RadioButtonTabs_Click(object sender, RoutedEventArgs e)
+        {
+            var radioButton = sender as RadioButton;
+
+            if (radioButton != null)
+            {
+                int index = Convert.ToInt32(radioButton.Tag);
+                FlipViewTabs.SelectedIndex = index;
+            }
+        }
+
+        private void NewSpriteFlyout_OnOpen(object sender, object e)
         {
             var viewModel = ServiceLocator.GetInstance<AddNewSpriteViewModel>();
             viewModel.NavigationObject = (Flyout)sender;
         }
 
-        private void RadioButtonTabs_Click(object sender, RoutedEventArgs e)
-        {
-            var radBtn = sender as RadioButton;
-
-            if (radBtn != null)
-            {
-                int index = Convert.ToInt32(radBtn.Tag);
-
-                FlipViewTabs.SelectedIndex = index;
-            }
-        }
-
-        private void FlyoutChangeName_OnOpening(object sender, object e)
+        private void FlyoutChangeSprite_OnOpening(object sender, object e)
         {
             var viewModel = ServiceLocator.GetInstance<ChangeSpriteViewModel>();
             viewModel.NavigationObject = (Flyout)sender;
@@ -58,37 +83,53 @@ namespace Catrobat.IDE.Store.Views.Editor.Sprites
             viewModel.NavigationObject = (Flyout)sender;
         }
 
+        private void NewSoundFlyout_OnOpening(object sender, object e)
+        {
+            var viewModel = ServiceLocator.GetInstance<NewSoundSourceSelectionViewModel>();
+            viewModel.NavigationObject = (Flyout)sender;
+        }
+
+
         private void GridViewActions_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            bool showAppbar =
-                _spriteEditorViewModel.SelectedActions.Count - e.RemovedItems.Count > 0 ||
-                _spriteEditorViewModel.SelectedCostumes.Count > 0 ||
-                _spriteEditorViewModel.SelectedSounds.Count > 0 ||
-                e.AddedItems.Count > 0;
+            AppBarBottomn.IsOpen = GridViewActions.SelectedItems.Count -
+                e.RemovedItems.Count + e.AddedItems.Count > 0;
 
-            AppBarBottomn.IsOpen = showAppbar;
+            if (ItemsControlAppBars.Items != null)
+            {
+                ItemsControlAppBars.Items.Remove(_appBarActions);
+                ItemsControlAppBars.Items.Remove(_appBarLooks);
+                ItemsControlAppBars.Items.Remove(_appBarSounds);
+                ItemsControlAppBars.Items.Add(_appBarActions);
+            }
         }
 
         private void GridViewLooks_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            bool showAppbar =
-                _spriteEditorViewModel.SelectedActions.Count > 0 ||
-                _spriteEditorViewModel.SelectedCostumes.Count - e.RemovedItems.Count > 0 ||
-                _spriteEditorViewModel.SelectedSounds.Count > 0 ||
-                e.AddedItems.Count > 0;
+            AppBarBottomn.IsOpen = _spriteEditorViewModel.SelectedCostumes.Count - 
+                e.RemovedItems.Count + e.AddedItems.Count > 0;
 
-            AppBarBottomn.IsOpen = showAppbar;
+            if (ItemsControlAppBars.Items != null)
+            {
+                ItemsControlAppBars.Items.Remove(_appBarActions);
+                ItemsControlAppBars.Items.Remove(_appBarLooks);
+                ItemsControlAppBars.Items.Remove(_appBarSounds);
+                ItemsControlAppBars.Items.Add(_appBarLooks);
+            }
         }
 
         private void GridViewSounds_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            bool showAppbar =
-                _spriteEditorViewModel.SelectedActions.Count > 0 ||
-                _spriteEditorViewModel.SelectedCostumes.Count > 0 ||
-                _spriteEditorViewModel.SelectedSounds.Count - e.RemovedItems.Count > 0 ||
-                e.AddedItems.Count > 0;
+            AppBarBottomn.IsOpen = GridViewSounds.SelectedItems.Count -
+                e.RemovedItems.Count + e.AddedItems.Count > 0;
 
-            AppBarBottomn.IsOpen = showAppbar;
+            if (ItemsControlAppBars.Items != null)
+            {
+                ItemsControlAppBars.Items.Remove(_appBarActions);
+                ItemsControlAppBars.Items.Remove(_appBarLooks);
+                ItemsControlAppBars.Items.Remove(_appBarSounds);
+                ItemsControlAppBars.Items.Add(_appBarSounds);
+            }
         }
 
         private void TileGeneratorFlyout_OnOpened(object sender, object e)
@@ -108,6 +149,91 @@ namespace Catrobat.IDE.Store.Views.Editor.Sprites
             // TODO: maybe reset ViewModel
 
             ServiceLocator.NavigationService.NavigateBack();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (_spritesViewModel.SelectedSprite == null && _spriteEditorViewModel.Sprites.Count > 0)
+                _spritesViewModel.SelectedSprite = _spriteEditorViewModel.Sprites[0];
+
+            base.OnNavigatedTo(e);
+        }
+
+        private void GridViewSprites_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.RemovedItems.Count > 0 && e.AddedItems.Count == 0)
+                _spritesViewModel.SelectedSprite = (Sprite)e.RemovedItems[0];
+        }
+
+        //#region Drag operations
+
+        //private void GridViewLooks_OnDragItemsStarting(object sender, DragItemsStartingEventArgs e)
+        //{
+        //    _spriteEditorViewModel.SelectedCostumes = new ObservableCollection<Costume>();
+
+        //    foreach (Costume item in e.Items)
+        //    {
+        //        _spriteEditorViewModel.SelectedCostumes.Add(item);
+        //    }
+        //}
+
+        //private void GridBasket_OnDragEnter(object sender, DragEventArgs e)
+        //{
+        //    GridDelete.Background = new SolidColorBrush(Colors.Green);
+        //}
+
+        //private void GridBasket_OnDragLeave(object sender, DragEventArgs e)
+        //{
+        //    GridDelete.Background = new SolidColorBrush(Colors.DarkSlateGray);
+        //}
+
+        //private void GridBasket_OnDrop(object sender, DragEventArgs e)
+        //{
+        //    var f = e.Data.GetView().AvailableFormats;
+
+        //    GridDelete.Background = new SolidColorBrush(Colors.DarkSlateGray);
+
+        //    switch (FlipViewTabs.SelectedIndex)
+        //    {
+        //        case 0:
+        //            _spriteEditorViewModel.DeleteScriptBrickCommand.Execute(null);
+        //            break;
+
+        //        case 1:
+
+        //            _spriteEditorViewModel.DeleteCostumeCommand.Execute(null);
+        //            break;
+
+        //        case 2:
+        //            _spriteEditorViewModel.DeleteSoundCommand.Execute(null);
+        //            break;
+        //    }
+        //}
+
+        //#endregion
+
+        private void FlipViewTabs_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AppBarBottomn == null)
+                return;
+
+            switch (((FlipView)sender).SelectedIndex)
+            {
+                case 0:
+                    RadioButtonActions.IsChecked = true;
+                    AppBarBottomn.IsOpen = _spriteEditorViewModel.SelectedActions.Count > 0;
+                    break;
+
+                case 1:
+                    RadioButtonLooks.IsChecked = true;
+                    AppBarBottomn.IsOpen = _spriteEditorViewModel.SelectedCostumes.Count > 0;
+                    break;
+
+                case 2:
+                    RadioButtonSounds.IsChecked = true;
+                    AppBarBottomn.IsOpen = _spriteEditorViewModel.SelectedSounds.Count > 0;
+                    break;
+            }
         }
     }
 }
