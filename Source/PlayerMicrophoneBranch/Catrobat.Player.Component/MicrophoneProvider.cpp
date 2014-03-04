8926574149d8bb8d5ff91755b39f429ec9ea85e0
Christopher Immervoll <CATROBAT\christopher.imm>
2014-03-04 19:40:04 +0100

    added fmod microphone provider

--- Source/PlayerMicrophoneBranch/Catrobat.Player.Component/MicrophoneProvider.cpp
+++ Source/PlayerMicrophoneBranch/Catrobat.Player.Component/MicrophoneProvider.cpp
@@ -1,10 +1,11 @@
 #include "pch.h"
 #include "MicrophoneProvider.h"
+#include <ppltasks.h>
 
 using namespace Windows::Foundation;
 using namespace Windows::Phone::Media::Capture;
 using namespace Windows::Storage::Streams;
-
+/*
 MicrophoneProvider::MicrophoneProvider()
 {
 	Init();
@@ -28,7 +29,8 @@ bool MicrophoneProvider::stopRecording()
 
 bool MicrophoneProvider::Init()
 {
-	//m_audio_device = AudioVideoCaptureDevice::OpenForAudioOnlyAsync();
+	m_microphoneDevice = AudioVideoCaptureDevice::OpenForAudioOnlyAsync();
+	m_audio_device = ref new AudioVideoCaptureDevice();
 	auto success = false;
 
 	if (m_audio_device != nullptr)
@@ -37,3 +39,4 @@ bool MicrophoneProvider::Init()
 	}
 	return success;
 }
+*/
--- Source/PlayerMicrophoneBranch/Catrobat.Player.Component/MicrophoneProvider.h
+++ Source/PlayerMicrophoneBranch/Catrobat.Player.Component/MicrophoneProvider.h
@@ -1,19 +1,21 @@
 #pragma once
 
+using namespace Windows::Phone::Media::Capture;
+
 class MicrophoneProvider
 {
 public:
 	MicrophoneProvider();
 	~MicrophoneProvider();
 
-	bool startRecording();
-	bool stopRecording();
+	//bool startRecording();
+	//bool stopRecording();
 
-private:
-	//IAsyncOperation<AudioVideoCaptureDevice^>^ m_audio_device;
-	Windows::Phone::Media::Capture::AudioVideoCaptureDevice^ m_audio_device;
+private:/*
+	IAsyncOperation<AudioVideoCaptureDevice^>^ m_microphoneDevice;
+	AudioVideoCaptureDevice^ m_audio_device;
 	Windows::Storage::Streams::IRandomAccessStream^ m_stream;
 	bool Init();
 
-
+	*/
 };
