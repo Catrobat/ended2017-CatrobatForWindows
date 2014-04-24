d3888f5ba2bf023b6a9f67c8feed26c10dfe8e52
Karl Koch <CATROBAT\karl.koch>
2014-04-24 15:43:39 +0200

    Added tests for VariableBricks. Simplified test of Variables. Added one helper function for converting a string to double. Modified ChangeVariableBrick and ProjectDaemon. ***NO_CI***

--- Source/Master/Catrobat/Catrobat.Player.Component/Helper.cpp
+++ Source/Master/Catrobat/Catrobat.Player.Component/Helper.cpp
@@ -1,14 +1,17 @@
 #include "pch.h"
+#include <string>
 #include "Helper.h"
+#include <algorithm>
+#include <limits.h>
 
-string Helper::ConvertPlatformStringToString(String^ input)
+std::string Helper::ConvertPlatformStringToString(Platform::String^ input)
 {
-	wstring foo(input->Begin());
-	string res(foo.begin(), foo.end());
+    std::wstring foo(input->Begin());
+    std::string res(foo.begin(), foo.end());
 	return res;
 }
 
-String^ Helper::ConvertStringToPlatformString(string input)
+Platform::String^ Helper::ConvertStringToPlatformString(std::string input)
 {
     std::string s_str = std::string(input);
     std::wstring wid_str = std::wstring(s_str.begin(), s_str.end());
@@ -16,8 +19,21 @@ String^ Helper::ConvertStringToPlatformString(string input)
     return ref new Platform::String(w_char);
 }
 
-string Helper::RetrieveClassName(string input)
+std::string Helper::RetrieveClassName(std::string input)
 {
-    string className = input.erase(0, input.find(" "));
+    std::string className = input.erase(0, input.find(" "));
     return className;
+}
+
+bool Helper::ConvertStringToDouble(std::string s, double* d)
+{   
+    char* check = NULL;
+
+    // TODO to check: unexpected behaviour for DBL_MAX and DBL_MIN in s
+    *d = std::strtod(s.c_str(), &check);
+
+    if (check == NULL || *check != '\0' || !(*d <= DBL_MAX && *d >= -DBL_MAX))
+        return false;
+    
+    return true;
 }
