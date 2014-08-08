2aba94e19e8b621658a3c7726dd314465430998e
Valentin Rock <CATROBAT\valentin.rock>
2014-08-09 00:54:40 +0200

    Fixed bug in LookNameChooserView

--- Source/WindowsUnified/Catrobat.IDE/Catrobat.IDE.Windows/Catrobat.IDE.WindowsPhone/Views/Editor/Looks/LookNameChooserView.xaml.cs
+++ Source/WindowsUnified/Catrobat.IDE/Catrobat.IDE.Windows/Catrobat.IDE.WindowsPhone/Views/Editor/Looks/LookNameChooserView.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Windows.UI.Xaml;
+﻿using System.Diagnostics;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Navigation;
 using Catrobat.IDE.Core.Services;
+using Catrobat.IDE.Core.UI;
 using Catrobat.IDE.Core.ViewModels;
 using Catrobat.IDE.Core.ViewModels.Editor.Looks;
 using Windows.UI.Xaml.Controls;
@@ -11,13 +13,13 @@ namespace Catrobat.IDE.WindowsPhone.Views.Editor.Looks
     {
         private readonly LookNameChooserViewModel _viewModel =
             ServiceLocator.ViewModelLocator.LookNameChooserViewModel;
-        
+
 
         public LookNameChooserView()
         {
             InitializeComponent();
 
-                Loaded += OnLoaded;
+            Loaded += OnLoaded;
         }
 
         private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
@@ -35,5 +37,21 @@ namespace Catrobat.IDE.WindowsPhone.Views.Editor.Looks
         {
             _viewModel.LookName = TextBoxLookName.Text;
         }
+
+        //private void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    var listView = (ListViewBase)sender;
+
+        //    if (e.AddedItems != null && e.AddedItems.Count >= 1)
+        //        ((ISelectable)e.AddedItems[0]).IsSelected = true;
+
+        //    if (e.RemovedItems != null && e.RemovedItems.Count >= 1)
+        //        ((ISelectable)e.RemovedItems[0]).IsSelected = false;
+
+        //    if (!listView.SelectedItems.Contains(_viewModel.SelectedSize))
+        //    {
+        //        Debugger.Break();
+        //    }
+        //}
     }
 }
