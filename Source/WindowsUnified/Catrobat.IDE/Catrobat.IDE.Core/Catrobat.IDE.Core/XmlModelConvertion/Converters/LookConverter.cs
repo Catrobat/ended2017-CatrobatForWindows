308424dd06e2d4c6f86633e11ef1a3b9ed3de824
Valentin Rock <CATROBAT\valentin.rock>
2014-08-23 21:46:37 +0200

    Added some Xml <-> Model converters

---
+++ Source/WindowsUnified/Catrobat.IDE/Catrobat.IDE.Core/Catrobat.IDE.Core/XmlModelConvertion/Converters/LookConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catrobat.IDE.Core.Models;
+using Catrobat.IDE.Core.Xml.XmlObjects;
+
+namespace Catrobat.IDE.Core.XmlModelConvertion.Converters
+{
+    public class LookConverter : XmlModelConverter<XmlLook, Look>
+    {
+        public override Look Convert(XmlLook o)
+        {
+            return new Look
+            {
+                Name = o.Name,
+                FileName = o.FileName
+            };
+        }
+
+        public override XmlLook Convert(Look m)
+        {
+            return new XmlLook
+            {
+                Name = m.Name,
+                FileName = m.FileName
+            };
+        }
+    }
+}
---
+++ Source/WindowsUnified/Catrobat.IDE/Catrobat.IDE.Core/Catrobat.IDE.Core/XmlModelConvertion/Converters/SoundConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catrobat.IDE.Core.Models;
+using Catrobat.IDE.Core.Xml.XmlObjects;
+
+namespace Catrobat.IDE.Core.XmlModelConvertion.Converters
+{
+    public class SoundConverter : XmlModelConverter<XmlSound, Sound>
+    {
+        public override Sound Convert(XmlSound o)
+        {
+            return new Sound
+            {
+                Name = o.Name,
+                FileName = o.FileName
+            };
+        }
+
+        public override XmlSound Convert(Sound m)
+        {
+            return new XmlSound
+            {
+                Name = m.Name,
+                FileName = m.FileName
+            };
+        }
+    }
+}
