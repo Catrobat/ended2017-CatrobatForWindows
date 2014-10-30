8f81a316caab94d6fee504caa32e38f3b948d03f
David Prott <CATROBAT\david.prott>
2014-10-30 17:30:15 +0100

    [Claudio,David] Ellipse Tool should now work properly after a reset.

--- Source/WindowsUnified/Catrobat.Paint/Catrobat.Paint.Windows/Catrobat.Paint.WindowsPhone/Tool/EllipseTool.cs
+++ Source/WindowsUnified/Catrobat.Paint/Catrobat.Paint.Windows/Catrobat.Paint.WindowsPhone/Tool/EllipseTool.cs
@@ -84,6 +84,8 @@ namespace Catrobat.Paint.WindowsPhone.Tool
         {
             PocketPaintApplication.GetInstance().GridEllipseSelectionControl.Children.Clear();
             PocketPaintApplication.GetInstance().GridEllipseSelectionControl.Children.Add(new EllipseSelectionControl());
+            PocketPaintApplication.GetInstance().BarRecEllShape.setTbHeightValue = 160.0;
+            PocketPaintApplication.GetInstance().BarRecEllShape.setTbWidthValue = 160.0;
         }
     }
 }
