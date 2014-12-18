6918de6d4d30b2dbd92196df14b2545c845f8896
Karl Koch <CATROBAT\karl.koch>
2014-12-18 15:29:32 +0100

    Merged back intermediate branch of IDE (IDEPlayer)

--- Source/WindowsUnified/Catrobat.IDE/Catrobat.IDE.Windows/Catrobat.IDE.WindowsPhone/Views/Main/PlayerView.xaml.cs
+++ Source/WindowsUnified/Catrobat.IDE/Catrobat.IDE.Windows/Catrobat.IDE.WindowsPhone/Views/Main/PlayerView.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-//using System.Collections.Generic;
 using System.IO;
-//using System.Linq;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices.WindowsRuntime;
-using Catrobat.IDE.Core;
-using Catrobat.IDE.Core.Services;
-using Catrobat.IDE.Core.ViewModels.Main;
-//using Windows.Foundation;
-//using Windows.Foundation.Collections;
-//using Windows.UI.Xaml;
-//using Windows.UI.Xaml.Controls;
-//using Windows.UI.Xaml.Controls.Primitives;
-//using Windows.UI.Xaml.Data;
-//using Windows.UI.Xaml.Input;
-//using Windows.UI.Xaml.Media;
+
 using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.Foundation.Collections;
@@ -23,16 +11,12 @@ using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.ApplicationModel.Activation;
 
+using Catrobat.IDE.Core;
+using Catrobat.IDE.Core.Services;
+using Catrobat.IDE.Core.ViewModels.Main;
+using Catrobat.IDE.WindowsShared.Services;
 using Catrobat_Player;
-//using Catrobat.IDE.Core.CatrobatObjects;
-//using Catrobat.IDE.Core.Resources.Localization;
-//using Catrobat.IDE.Core.ViewModels;
 
-//using Windows.System;
-//using Windows.UI.Xaml.Controls;
-
-//using Windows.UI.Xaml.Navigation;
-//using Catrobat.IDE.WindowsShared.Misc;
 
 namespace Catrobat.IDE.WindowsPhone.Views.Main
 {
@@ -40,16 +24,15 @@ namespace Catrobat.IDE.WindowsPhone.Views.Main
     {
         private readonly PlayerViewModel _viewModel =
             ServiceLocator.ViewModelLocator.PlayerViewModel;
-        private Catrobat_Player.Catrobat_PlayerAdapter _playerObject =
+        private Catrobat_Player.Catrobat_PlayerAdapter _playerObject = 
             new Catrobat_Player.Catrobat_PlayerAdapter();
 
         public PlayerView()
         {
             InitializeComponent();
-
         }
-
-
+        
+        
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             //SetSourceOfThumbnail(); --> TODO it is necessary to change the SplashScreen image instead of setting an image on this XAML page
@@ -59,12 +42,12 @@ namespace Catrobat.IDE.WindowsPhone.Views.Main
                     ServiceLocator.NavigationService.RemoveBackEntry();
 
             _playerObject.InitPlayer(swapChainPanel, PlayerAppBar, _viewModel.ProjectName);
-            _viewModel.PlayerObject = _playerObject;
+            PlayerLauncherServiceWindowsShared.SetPlayerObject(_playerObject);
 
             base.OnNavigatedTo(e);
         }
 
-
+       
 
         /*private async void SetSourceOfThumbnail()
         {
