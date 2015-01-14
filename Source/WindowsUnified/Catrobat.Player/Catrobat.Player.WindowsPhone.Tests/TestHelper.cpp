7ff36cd5709d5f1278066c6d70c52ce2c5f42e35
Hannes Hasenauer <CATROBAT\hannes.hasenauer>
2015-01-14 15:46:21 +0100

    test refactoring

--- Source/WindowsUnified/Catrobat.Player/Catrobat.Player.WindowsPhone.Tests/TestHelper.cpp
+++ Source/WindowsUnified/Catrobat.Player/Catrobat.Player.WindowsPhone.Tests/TestHelper.cpp
@@ -1,20 +1,6 @@
 #include "pch.h"
 #include "TestHelper.h"
 
-bool TestHelper::isEqual(float x, float y)
-{
-	if (x <= y + EPSILON && x >= y - EPSILON)
-		return true;
-	return false;
-}
-
-bool TestHelper::isEqual(double x, double y)
-{
-	if (x <= y + EPSILON && x >= y - EPSILON)
-		return true;
-	return false;
-}
-
 std::string TestHelper::ConvertPlatformStringToString(Platform::String^ input)
 {
     std::wstring foo(input->Begin());
--- Source/WindowsUnified/Catrobat.Player/Catrobat.Player.WindowsPhone.Tests/TestHelper.h
+++ Source/WindowsUnified/Catrobat.Player/Catrobat.Player.WindowsPhone.Tests/TestHelper.h
@@ -5,9 +5,6 @@
 class TestHelper
 {
 public:
-	static bool isEqual(float x, float y);
-    static bool isEqual(double x, double y);
-
     static std::string ConvertPlatformStringToString(Platform::String^ input);
     static Platform::String^ ConvertStringToPlatformString(std::string input);
 };
