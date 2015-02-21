389abc21a2a78fe5cd5c7fa0eea11a9f46885f2b
Karl Koch <CATROBAT\karl.koch>
2015-02-21 21:23:14 +0100

    [IDE-PlayerIntegration] Set project's thumbnail as SplashScreen when the project gets loaded

--- Source/WindowsUnified/Catrobat.IDE/Catrobat.IDE.Windows/Catrobat.IDE.Shared/Services/PlayerLauncherServiceWindowsShared.cs
+++ Source/WindowsUnified/Catrobat.IDE/Catrobat.IDE.Windows/Catrobat.IDE.Shared/Services/PlayerLauncherServiceWindowsShared.cs
@@ -10,6 +10,7 @@ using Windows.UI.Xaml.Media.Imaging;
 using GalaSoft.MvvmLight.Messaging;
 
 using Catrobat_Player;
+using Catrobat.IDE.Core;
 using Catrobat.IDE.Core.ViewModels;
 using Catrobat.IDE.Core.ViewModels.Main;
 using Catrobat.IDE.Core.Services;
@@ -66,24 +67,103 @@ namespace Catrobat.IDE.WindowsShared.Services
         private async Task ShowSplashScreen(string projectName)
         {
             // TODO use project's thumbnail/screenshot as SplashScreen image
-            // TODO check: global constant for "ms-appx:///Assets/SplashScreen.png"?
             // TODO duplicate of this method (again in App.xaml.cs) --> put it to some global 
-            //      space where it can be accessed from both classes
+            //      space where it can be accessed from both classes?!?
 
-            var file = await StorageFile.GetFileFromApplicationUriAsync(
-                new Uri("ms-appx:///Assets/SplashScreen.png", UriKind.Absolute));
+            pageFrame = (Frame)Window.Current.Content;
+            var splashImage = await GetThumbnailFromProject(projectName);
+            Window.Current.Content = new ExtendedSplash(splashImage);
+            Window.Current.Activate();
+        }
+
+        private async Task<BitmapImage> GetThumbnailFromProject(string projectName)
+        {
+            // TODO check: global constant for "ms-appx:///Assets/SplashScreen.png" or "ms-appdata:///Local/"?
+
+            var projectFolder = "ms-appdata:///Local/" + StorageConstants.ProgramsPath + 
+                                "/" + projectName + "/";
+            StorageFile file;
+
+            // TODO: problem with getting manual_screenshot.png, even if the file is present...and it is possible to access automatic_screenshot..
+            // firstly, try to get manual_screenshot
+            var manualScreenshotPath = projectFolder + StorageConstants.ProgramManualScreenshotPath;
+            try
+            {
+                file = await StorageFile.GetFileFromApplicationUriAsync(
+                        new Uri(manualScreenshotPath, UriKind.Absolute));
+            }
+            catch (FileNotFoundException e)
+            {
+                file = null;
+            }
+
+            // secondly, if the manual_screenshot is not available, try to get automatic_screenshot
+            if (file == null)
+            {
+                var automaticProjectScreenshotPath = projectFolder + 
+                                                    StorageConstants.ProgramAutomaticScreenshotPath;
+                try
+                {
+                    file = await StorageFile.GetFileFromApplicationUriAsync(
+                            new Uri(automaticProjectScreenshotPath, UriKind.Absolute));
+                }
+                catch (FileNotFoundException e)
+                {
+                    file = null;
+                }
+            }
+
+            // if neither of them is available --> get default SplashScreen image
+            if (file == null)
+            {
+                file = await StorageFile.GetFileFromApplicationUriAsync(
+                        new Uri("ms-appx:///Assets/SplashScreen.png", UriKind.Absolute));
+            }
+
+
             var randomAccessStream = await file.OpenReadAsync();
-
             var splashImage = new BitmapImage()
             {
                 CreateOptions = BitmapCreateOptions.None
             };
             await splashImage.SetSourceAsync(randomAccessStream);
 
-            pageFrame = (Frame)Window.Current.Content;
-            Window.Current.Content = new ExtendedSplash(splashImage);
-            Window.Current.Activate();
+            return splashImage;
         }
+
+        // here is an old code for getting the screenshot/thumbnail out of the
+        /*private async void SetSourceOfThumbnail()
+       {
+           var projectFolder = "ms-appdata:///Local/" + StorageConstants.ProgramsPath + "/" + _viewModel.ProjectName + "/";
+
+           StorageFile file = null;
+           try
+           {
+               var manualScreenshotPath = projectFolder + StorageConstants.ProgramManualScreenshotPath;
+               file = await StorageFile.GetFileFromApplicationUriAsync(
+                       new Uri(manualScreenshotPath, UriKind.Absolute));
+           } 
+           catch (Exception e)
+           {
+               file = null;                
+           }
+
+           if (file == null)
+           {
+               try
+               {
+                   var automaticProjectScreenshotPath = projectFolder + StorageConstants.ProgramAutomaticScreenshotPath;
+                   file = await StorageFile.GetFileFromApplicationUriAsync(
+                           new Uri(automaticProjectScreenshotPath, UriKind.Absolute));
+               }
+               catch (Exception e)
+               {
+                   file = null;
+               }
+           }
+
+           
+       } */
 
         public static void SetPlayerObject(Catrobat_PlayerAdapter playerObject)
         {
