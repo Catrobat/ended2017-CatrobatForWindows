b8b52eb235c1e2ae52663f97d378d675258abfa3
Philipp Purgaj <CATROBAT\philipp.purgaj>
2015-04-09 00:07:06 +0200

    WPH-29 Crop Tool: created correct project and method skeletons

---
+++ Source/WindowsUnified/Catrobat.Paint/Catrobat.Paint.Windows/Catrobat.Paint.WindowsPhone.Tests/CropToolTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Catrobat.Paint.WindowsPhone.Controls.UserControls;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Catrobat.Paint.WindowsPhone.Tests
+{
+    [TestClass]
+    public class CropToolTests
+    {
+        [TestMethod]
+        public void AddWriteableBitmapToCanvasTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ChangeHeightOfUiElementsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ChangeMarginBottomOfUiElementsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ChangeMarginLeftOfUiElementsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ChangeMarginRightOfUiElementsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ChangeMarginTopOfUiElementsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void rectRectangleCropSelection_ManipulationDeltaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void rectRectangleCropSelection_DoubleTappedTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ResetAppBarButtonRectangleSelectionControlTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void HasElementsPaintingAreaViewsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetRectangleCropSelectionHeightTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetRectangleCropSelectionWidthTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetLeftTopCoordinateRectangleCropSelectionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SetLeftTopNullPointCropSelectionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetLeftTopNullPointCropSelectionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void CropImageTest()
+        {
+            Assert.Fail();
+        }
+
+    }
+}
+
--- Source/WindowsUnified/Catrobat.Paint/Catrobat.Paint.Windows/Catrobat.Paint.WindowsPhone.Tests/Properties/AssemblyInfo.cs
+++ Source/WindowsUnified/Catrobat.Paint/Catrobat.Paint.Windows/Catrobat.Paint.WindowsPhone.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Catrobat.Paint.WindowsPhone.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -26,3 +26,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
