b4d56c4443c82ad1591adb279426e97d92a41a6f
Philipp Purgaj <CATROBAT\philipp.purgaj>
2015-04-28 18:56:49 +0200

    WPH-29 Crop Tool Tests: Wrapper function for using UIThread, refactoring SetControlSize Tests and created Tests for SetHeightOfVerticalCornerRectangle

--- Source/WindowsUnified/Catrobat.Paint/Catrobat.Paint.Windows/Catrobat.Paint.WindowsPhone.Tests/CropToolTests.cs
+++ Source/WindowsUnified/Catrobat.Paint/Catrobat.Paint.Windows/Catrobat.Paint.WindowsPhone.Tests/CropToolTests.cs
@@ -1,14 +1,44 @@
 ﻿using System;
+using System.Linq;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Catrobat.Paint.WindowsPhone.Controls.UserControls;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Shapes;
 
 namespace Catrobat.Paint.WindowsPhone.Tests
 {
     [TestClass]
     public class CropToolTests
     {
+        private PocketPaintApplication app;
+        private CropControl sut;
+
+        [TestInitialize]
+        [Timeout(2000)]
+        public void Init()
+        {
+            Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(
+                CoreDispatcherPriority.Normal,
+                () =>
+                {
+                    app = PocketPaintApplication.GetInstance();                
+                    app.CropControl = new CropControl();
+                    sut = app.CropControl;
+                });
+            while (sut == null) { }
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            sut = null;
+            app = null;
+        }
+
         [TestMethod]
         [Ignore]
         public void AddWriteableBitmapToCanvasTest()
@@ -18,72 +48,217 @@ namespace Catrobat.Paint.WindowsPhone.Tests
 
         [TestMethod]
         [TestCategory("SetControlSize")]
-        public void SetControlSizeTest()
+        public async Task SetControlSizeTest()
         {
-            Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
-            () =>
+            await ExecuteOnUIThread(() =>
             {
-                PocketPaintApplication app = PocketPaintApplication.GetInstance();
-                app.CropControl = new CropControl();
-                CropControl sut = app.CropControl;              
-                sut.SetControlSize(300, 160);
-            }
-            );
-        }
+                var height = 300;
+                var width = 160;
 
-        [TestMethod]
-        [TestCategory("SetControlSize")]
-        public void SetControlSizeMaxValueTest()
-        {
-            Assert.ThrowsException<ArgumentException>(() =>
-            Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
-            () =>
-            {
-                PocketPaintApplication app = PocketPaintApplication.GetInstance();
-                app.CropControl = new CropControl();
-                CropControl sut = app.CropControl;
-                try
+                sut.SetControlSize(height, width);
+                Grid grid = sut.Content as Grid;
+                if (grid != null)
                 {
-                    sut.SetControlSize(double.MaxValue, double.MaxValue);
+                    Assert.AreEqual(grid.Width, width);
+                    Assert.AreEqual(grid.Height, height);
                 }
-                catch (Exception ex)
-                {
-                    throw new ArgumentException();
-                }
-            }
-            ));
+            });                   
         }
 
         [TestMethod]
         [TestCategory("SetControlSize")]
-        public void SetControlSizeMinValueTest()
+        public async Task SetControlSizeMaxValueTest()
         {
-            Assert.ThrowsException<ArgumentException>(() =>
-            Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
-            () =>
+            await ExecuteOnUIThread(() =>
             {
-                PocketPaintApplication app = PocketPaintApplication.GetInstance();
-                app.CropControl = new CropControl();
-                CropControl sut = app.CropControl;
-                sut.SetControlSize(double.MinValue, double.MinValue);
-            }
-            ));
+                Assert.ThrowsException<ArgumentException>(() =>
+                    sut.SetControlSize(double.MaxValue, double.MaxValue));
+
+            });
         }
 
         [TestMethod]
         [TestCategory("SetControlSize")]
-        public void SetControlSizeNaNValueTest()
+        public async Task SetControlSizeMinValueTest()
         {
-            Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
-            () =>
+            await ExecuteOnUIThread(() =>
             {
-                PocketPaintApplication app = PocketPaintApplication.GetInstance();
-                app.CropControl = new CropControl();
-                CropControl sut = app.CropControl;
-                sut.SetControlSize(double.NaN, double.NaN);
-            }
-            );
+                Assert.ThrowsException<ArgumentException>(() =>
+                sut.SetControlSize(double.MinValue, double.MinValue));
+
+            });
         }
+
+
+        
+
+        [TestMethod]
+        [TestCategory("SetHeightOfVerticalCornerRectangles")]
+        public async Task SetHeightOfVerticalCornerRectanglesTest()
+        {
+            await ExecuteOnUIThread(() =>
+            {
+                double value = 20.0;
+                sut.SetHeightOfVerticalCornerRectangles(value);
+                var maingrid = sut.Content as Grid;
+                if (maingrid != null)
+                    foreach (var uiElement1 in maingrid.Children)
+                    {
+                        var uiElement = (Grid) uiElement1;
+                        foreach (var rectangle in uiElement.Children)
+                        {
+                            var child = (Rectangle) rectangle;
+                            switch (child.Name)
+                            {
+                                case "rectLeftBottomVert":
+                                case "rectLeftTopVert":
+                                case "rectRightBottomVert":
+                                case "rectRightTopVert":
+                                    Assert.AreEqual(child.Height, value);
+                                    break;
+                                default:
+                                    continue;
+
+                            }
+                        }
+                    }
+            });
+        }
+
+        [TestMethod]
+        [TestCategory("SetHeightOfVerticalCornerRectangles")]
+        public async Task SetHeightOfVerticalCornerRectanglesHighValueTest()
+        {
+            await ExecuteOnUIThread(() =>
+            {
+                double value = 31.0;
+                double maxValue = 30.0;
+                sut.SetHeightOfVerticalCornerRectangles(value);
+                var maingrid = sut.Content as Grid;
+                if (maingrid != null)
+                    foreach (var uiElement1 in maingrid.Children)
+                    {
+                        var uiElement = (Grid)uiElement1;
+                        foreach (var rectangle in uiElement.Children)
+                        {
+                            var child = (Rectangle)rectangle;
+                            switch (child.Name)
+                            {
+                                case "rectLeftBottomVert":
+                                case "rectLeftTopVert":
+                                case "rectRightBottomVert":
+                                case "rectRightTopVert":
+                                    Assert.AreEqual(child.Height, maxValue);
+                                    break;
+                                default:
+                                    continue;
+
+                            }
+                        }
+                    }
+            });
+        }
+
+        [TestMethod]
+        [TestCategory("SetHeightOfVerticalCornerRectangles")]
+        public async Task SetHeightOfVerticalCornerRectanglesLowValueTest()
+        {
+            await ExecuteOnUIThread(() =>
+            {
+                double value = 4.0;
+                double minValue = 5.0;
+                sut.SetHeightOfVerticalCornerRectangles(value);
+                var maingrid = sut.Content as Grid;
+                if (maingrid != null)
+                    foreach (var uiElement1 in maingrid.Children)
+                    {
+                        var uiElement = (Grid)uiElement1;
+                        foreach (var rectangle in uiElement.Children)
+                        {
+                            var child = (Rectangle)rectangle;
+                            switch (child.Name)
+                            {
+                                case "rectLeftBottomVert":
+                                case "rectLeftTopVert":
+                                case "rectRightBottomVert":
+                                case "rectRightTopVert":
+                                    Assert.AreEqual(child.Height, minValue);
+                                    break;
+                                default:
+                                    continue;
+
+                            }
+                        }
+                    }
+            });
+        }
+
+        [TestMethod]
+        [TestCategory("SetHeightOfVerticalCornerRectangles")]
+        public async Task SetHeightOfVerticalCornerRectanglesMaxValueTest()
+        {
+            await ExecuteOnUIThread(() =>
+            {
+                double maxValue = 30.0;
+                sut.SetHeightOfVerticalCornerRectangles(double.MaxValue);
+                var maingrid = sut.Content as Grid;
+                if (maingrid != null)
+                    foreach (var uiElement1 in maingrid.Children)
+                    {
+                        var uiElement = (Grid)uiElement1;
+                        foreach (var rectangle in uiElement.Children)
+                        {
+                            var child = (Rectangle)rectangle;
+                            switch (child.Name)
+                            {
+                                case "rectLeftBottomVert":
+                                case "rectLeftTopVert":
+                                case "rectRightBottomVert":
+                                case "rectRightTopVert":
+                                    Assert.AreEqual(child.Height, maxValue);
+                                    break;
+                                default:
+                                    continue;
+
+                            }
+                        }
+                    }
+            });
+        }
+
+        [TestMethod]
+        [TestCategory("SetHeightOfVerticalCornerRectangles")]
+        public async Task SetHeightOfVerticalCornerRectanglesMinValueTest()
+        {
+            await ExecuteOnUIThread(() =>
+            {
+                double minValue = 5.0;
+                sut.SetHeightOfVerticalCornerRectangles(double.MinValue);
+                var maingrid = sut.Content as Grid;
+                if (maingrid != null)
+                    foreach (var uiElement1 in maingrid.Children)
+                    {
+                        var uiElement = (Grid)uiElement1;
+                        foreach (var rectangle in uiElement.Children)
+                        {
+                            var child = (Rectangle)rectangle;
+                            switch (child.Name)
+                            {
+                                case "rectLeftBottomVert":
+                                case "rectLeftTopVert":
+                                case "rectRightBottomVert":
+                                case "rectRightTopVert":
+                                    Assert.AreEqual(child.Height, minValue);
+                                    break;
+                                default:
+                                    continue;
+
+                            }
+                        }
+                    }
+            });
+        }
+
 
         [TestMethod]
         [Ignore]
@@ -190,6 +365,15 @@ namespace Catrobat.Paint.WindowsPhone.Tests
         public void CropImageTest()
         {
             Assert.Fail();
+        }
+
+
+
+
+
+        public IAsyncAction ExecuteOnUIThread(DispatchedHandler action)
+        {
+            return Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, action);
         }
 
     }
