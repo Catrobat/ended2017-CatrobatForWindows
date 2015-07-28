16c95e6c354321c5c106bfab152905d2f4a534d5
David Prott <CATROBAT\david.prott>
2015-07-28 21:47:30 +0200

    WPH [Paint]Color-Picker: Added hex value input control to enter rgb values in hex format. Minor changes.

--- Source/WindowsUnified/Catrobat.Paint/Catrobat.Paint.Windows/Catrobat.Paint.WindowsPhone/Controls/UserControls/InputHexValueControl.xaml.cs
+++ Source/WindowsUnified/Catrobat.Paint/Catrobat.Paint.Windows/Catrobat.Paint.WindowsPhone/Controls/UserControls/InputHexValueControl.xaml.cs
@@ -19,24 +19,31 @@ namespace Catrobat.Paint.WindowsPhone.Controls.UserControls
 
         private void ButtonNumbers_Click(object sender, RoutedEventArgs e)
         {
-            if(tbInputValue.Text.Length <= 7)
+            if (tbInputValue.Text.Length < 6)
             {
                 Button button = sender as Button;
                 if (button != null)
                 {
                     string getTextOfClickedButton = button.Name.Substring(8);
-                        tbInputValue.Text += getTextOfClickedButton;
+                    tbInputValue.Text += getTextOfClickedButton;
                 }
+                changeColorOfCoding4ColorPickerByHexValueTextBox();
             }
-            
-            
-            int lenOfInputValue = tbInputValue.Text.Length;
-            if (lenOfInputValue == 6)
-            {
+        }
+
+        public void changeColorOfCoding4ColorPickerByHexValueTextBox()
+        {
+                int lenOfInputValue = tbInputValue.Text.Length;
                 Color color = new Color();
-                string redValueString = tbInputValue.Text.ToString().Substring(lenOfInputValue - 6, 2);
-                string greenValueString = tbInputValue.Text.ToString().Substring(lenOfInputValue - 4, 2);
-                string blueValueString = tbInputValue.Text.ToString().Substring(lenOfInputValue - 2, 2);
+                string leftRedByte = lenOfInputValue >= 1 ? tbInputValue.Text[0].ToString() : "F";
+                string rightRedByte = lenOfInputValue >= 2 ? tbInputValue.Text[1].ToString() : "F";
+                string redValueString = leftRedByte + rightRedByte;
+                string leftGreenByte = lenOfInputValue >= 3 ? tbInputValue.Text[2].ToString() : "F";
+                string rightGreenByte = lenOfInputValue >= 4 ? tbInputValue.Text[3].ToString() : "F";
+                string greenValueString = leftGreenByte + rightGreenByte;
+                string leftBlueByte = lenOfInputValue >= 5 ? tbInputValue.Text[4].ToString() : "F";
+                string rightBlueByte = lenOfInputValue >= 6 ? tbInputValue.Text[5].ToString() : "F";
+                string blueValueString = leftBlueByte + rightBlueByte;
                 color.R = Convert.ToByte(redValueString, 16);
                 color.G = Convert.ToByte(greenValueString, 16);
                 color.B = Convert.ToByte(blueValueString, 16);
@@ -46,12 +53,12 @@ namespace Catrobat.Paint.WindowsPhone.Controls.UserControls
                 PocketPaintApplication.GetInstance().ViewColorPicker.changeColorOfBtnSelectedColor(color);
                 PocketPaintApplication.GetInstance().ViewColorPicker.changeValuesOfColourSliders(color.R, color.G, color.B, color.A);
                 PocketPaintApplication.GetInstance().ViewColorPicker.changeValuesOfColourTextboxes(color.R, color.G, color.B, color.A);
-            }
         }
 
         private void btnDeleteDigit_Click(object sender, RoutedEventArgs e)
         {
             tbInputValue.Text = tbInputValue.Text.Length > 0 ? tbInputValue.Text.Substring(0, tbInputValue.Text.Length - 1) : tbInputValue.Text;
+            changeColorOfCoding4ColorPickerByHexValueTextBox();
         }
 
         private void tbInputValue_TextChanged(object sender, TextChangedEventArgs e)
