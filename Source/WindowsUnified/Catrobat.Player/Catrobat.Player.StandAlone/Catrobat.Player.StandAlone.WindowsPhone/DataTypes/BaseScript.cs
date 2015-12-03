0f8067c9ade90ed6db16e65b4cd0f2bc8c026254
Harald Schaffernak <CATROBAT\harald.schaffernak>
2015-12-03 17:43:13 +0100

    WPH-122 script base fixed.

--- Source/WindowsUnified/Catrobat.Player/Catrobat.Player.StandAlone/Catrobat.Player.StandAlone.WindowsPhone/DataTypes/BaseScript.cs
+++ Source/WindowsUnified/Catrobat.Player/Catrobat.Player.StandAlone/Catrobat.Player.StandAlone.WindowsPhone/DataTypes/BaseScript.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Catrobat_Player.NativeComponent;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,24 @@ using System.Xml.Serialization;
 namespace Catrobat.Player.StandAlone.DataTypes
 {
     [XmlInclude(typeof(WhenScript))]
-    public abstract class BaseScript
+    public abstract class BaseScript : IScript
     {
+        List<Brick> _bricks = new List<Brick>();
+
+        [XmlArray("brickList")]
+        [XmlArrayItemAttribute(typeof(TurnRightBrick))]
+        public List<Brick> Bricks
+        {
+            get { return _bricks; }
+            set { _bricks = value; }
+        }
+
+        [XmlIgnore]
+        IList<IBrick> IScript.Bricks
+        {
+            get { return Bricks.Cast<IBrick>().ToList(); }
+            set { throw new NotImplementedException(); }
+        }
     }
 
 }
--- Source/WindowsUnified/Catrobat.Player/Catrobat.Player.StandAlone/Catrobat.Player.StandAlone.WindowsPhone/DataTypes/WhenScript.cs
+++ Source/WindowsUnified/Catrobat.Player/Catrobat.Player.StandAlone/Catrobat.Player.StandAlone.WindowsPhone/DataTypes/WhenScript.cs
@@ -11,18 +11,8 @@ namespace Catrobat.Player.StandAlone.DataTypes
     [XmlType("whenScript")]
     public class WhenScript : BaseScript, IWhenScript
     {
-        List<Brick> _bricks = new List<Brick>();
-
         [XmlElement("action")]
         public string Action { get; set; }
-
-        [XmlArray("brickList")]
-        [XmlArrayItemAttribute(typeof(TurnRightBrick))]
-        public List<Brick> Bricks
-        {
-            get { return _bricks; }
-            set { _bricks = value; }
-        }
 
     }
 
