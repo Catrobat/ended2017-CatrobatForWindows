42a2dc3802af4451b28974e63299c16cad6d4ed7
Hannes Hasenauer <CATROBAT\hannes.hasenauer>
2016-01-13 12:43:37 +0100

    WPH-122 FormulaTree Interface

---
+++ Source/WindowsUnified/Catrobat.8.1/Catrobat/Catrobat.Player/Catrobat.Player.Shared/IFormulaTree.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace Catrobat_Player
+{
+	namespace NativeComponent
+	{
+		public interface class IFormulaTree
+		{
+		public:
+			virtual property IFormulaTree^ RightChild;
+			virtual property IFormulaTree^ LeftChild;
+			virtual property int Type;
+			virtual property Platform::String Value;
+			virtual property int Operator;
+			virtual property int Function;
+			virtual property int Sensor;
+		};
+	}
+}
