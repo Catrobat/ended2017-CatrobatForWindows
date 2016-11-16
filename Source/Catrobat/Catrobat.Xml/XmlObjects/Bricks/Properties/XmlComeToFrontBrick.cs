73908a8a263c81b054c5b68a893466e07c94af9a
Florian Schneider <florian.schneider@student.tugraz.at>
2016-11-16 12:30:06 +0100

    Temp

---
+++ Source/Catrobat/Catrobat.Xml/XmlObjects/Bricks/Properties/XmlComeToFrontBrick.cs
@@ -0,0 +1,44 @@
+﻿using System.Xml.Linq;
+using Catrobat_Player.NativeComponent;
+
+namespace Catrobat.IDE.Core.Xml.XmlObjects.Bricks.Properties
+{
+    public partial class XmlComeToFrontBrick : XmlBrick, IComeToFrontBrick
+    {
+        public override bool Equals(System.Object obj)
+        {
+            XmlComeToFrontBrick b = obj as XmlComeToFrontBrick;
+            if ((object)b == null)
+                return false;
+
+            return this.Equals(b);
+        }
+
+        public bool Equals(XmlComeToFrontBrick b)
+        {
+            return this.Equals((XmlBrick)b);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+
+        public XmlComeToFrontBrick() {}
+
+        public XmlComeToFrontBrick(XElement xElement) : base(xElement) {}
+
+        internal override void LoadFromXml(XElement xRoot) {}
+
+        internal override XElement CreateXml()
+        {
+            var xRoot = new XElement(XmlConstants.Brick);
+            xRoot.SetAttributeValue(XmlConstants.Type, XmlConstants.XmlComeToFrontBrickType);
+
+            //CreateCommonXML(xRoot);
+
+            return xRoot;
+        }
+    }
+}
