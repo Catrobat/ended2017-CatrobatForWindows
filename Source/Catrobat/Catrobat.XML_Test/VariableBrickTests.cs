8b57509865586d92defceb6f88c5729ff6d6acb3
Christopher Immervoll <christopher.immervoll@student.tugraz.at>
2016-12-05 17:20:19 +0100

    WPH-274 test to allow null reference for changeVariableBrick as initial state

---
+++ Source/Catrobat/Catrobat.XML_Test/VariableBrickTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Catrobat.IDE.Core;
+using Catrobat.IDE.Core.Xml.XmlObjects.Bricks.Properties;
+using Catrobat.IDE.Core.Xml.XmlObjects.Bricks.Variables;
+using Catrobat.IDE.Core.Xml.XmlObjects.Formulas;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Catrobat.XML_Test
+{
+    [TestClass]
+    public class VariableBrickTests
+    {
+        [TestMethod]
+        public void ChangeVariableBrickHasToAllowNullReferenceOfUserVariableAsInitialBrick()
+        {
+            TextReader sr = new StringReader("<brick type=\"ChangeVariableBrick\"><formulaList><formula category=\"VARIABLE_CHANGE\"><type>NUMBER</type><value>2</value></formula></formulaList></brick>");
+            var xRoot = XElement.Load(sr);
+
+            var testObject = new XmlChangeVariableBrick(xRoot);
+
+            var referenceObject = new XmlChangeVariableBrick()
+            {
+                VariableFormula = new XmlFormula()
+                {
+                    FormulaTree = new XmlFormulaTree()
+                    { 
+                        VariableType = "NUMBER",
+                        VariableValue = "2"
+                    }
+                },
+                UserVariable = null
+            };
+
+            Assert.AreEqual(referenceObject.CreateXml().ToString(), testObject.CreateXml().ToString());
+        }
+    }
+}
